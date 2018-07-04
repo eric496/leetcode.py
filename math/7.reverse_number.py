fe3e53542cc9184560c9932957f4d94c9312b85d
eric496 <eric.mlengineer@gmail.com>
2018-07-04 19:09:06 -0400

    add 7

---
+++ math/7.reverse_number.py
@@ -0,0 +1,28 @@
+'''
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+'''
+
+'''
+Thought:
+
+'''
+
+class Solution(object):
+    def reverse(self, x):
+        rev = -int(str(x)[1:][::-1]) if x < 0 else int(str(x)[::-1])
+        return rev if -2 ** 31 <= rev <= 2 ** 31 - 1 else 0
