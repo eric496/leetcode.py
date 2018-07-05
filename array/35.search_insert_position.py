e66651cbc314ef78b4c5dc81ab05dc1f2c1afe77
eric496 <eric.mlengineer@gmail.com>
2018-07-05 17:44:49 -0400

    add 35

---
+++ array/35.search_insert_position.py
@@ -0,0 +1,38 @@
+'''
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You may assume no duplicates in the array.
+
+Example 1:
+
+Input: [1,3,5,6], 5
+Output: 2
+Example 2:
+
+Input: [1,3,5,6], 2
+Output: 1
+Example 3:
+
+Input: [1,3,5,6], 7
+Output: 4
+Example 4:
+
+Input: [1,3,5,6], 0
+Output: 0
+'''
+
+# solution 1: one liner 
+class Solution:
+    def searchInsert(self, nums: list, target: int) -> int:
+        return len([n for n in nums if n < target])
+
+# solution 2: linear search
+class Solution:
+    def searchInsert(self, nums: list, target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        for n in nums:
+            if target < n:
+                return nums.index(n)
+        return len(nums)
+
+# solution 3: binary search
