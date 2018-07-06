06c94ef8337566d29244c5ae670d0e8b6b7ce8b3
eric496 <eric.mlengineer@gmail.com>
2018-07-06 16:47:04 -0400

    add 69

---
+++ math/69.sqrt(x).py
@@ -0,0 +1,31 @@
+'''
+Implement int sqrt(int x).
+Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+
+Example 1:
+
+Input: 4
+Output: 2
+Example 2:
+
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+'''
+
+'''
+Thought: 
+    Solution 1: Newton's method
+    Solution 2: Binary search
+'''
+
+# Newton's method
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        r = x
+        while r*r > x:
+            r = (r + x//r) // 2
+        return r
+
+# binary search
