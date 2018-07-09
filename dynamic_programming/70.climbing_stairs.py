1e3c26ca22bb8388e80327885e5f437df7b02d48
eric496 <eric.mlengineer@gmail.com>
2018-07-09 14:59:23 -0400

    add 70

---
+++ dynamic_programming/70.climbing_stairs.py
@@ -0,0 +1,45 @@
+'''
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+Note: Given n will be a positive integer.
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+'''
+
+'''
+Thought:
+
+'''
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 1:
+            return 1
+        if n == 2:
+            return 2
+        dp = [1, 2]
+        for i in range(2, n):
+            dp.append(dp[i-1] + dp[i-2])
+        return dp[n-1]
+
+# simplified solution
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a = b = 1
+        for _ in range(n):
+            a, b = b, a+b
+        return a
