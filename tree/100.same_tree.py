4e2fced701fc66c96d1969519013224ef617d8fb
eric496 <eric.mlengineer@gmail.com>
2018-07-10 16:34:03 -0400

    add iterative solution

--- tree/100.same_tree.py
+++ tree/100.same_tree.py
@@ -44,3 +44,20 @@ class Solution:
         if p and q:
             return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         return p is q
+
+# iterative solution
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        queue = [(p, q)]
+        while queue:
+            (node1, node2) = queue.pop(0)
+            if not node1 and not node2:
+                continue
+            elif None in [node1, node2]:
+                return False
+            else:
+                if node1.val != node2.val:
+                    return False
+                queue.append((node1.left, node2.left))
+                queue.append((node1.right, node2.right))
+        return True
