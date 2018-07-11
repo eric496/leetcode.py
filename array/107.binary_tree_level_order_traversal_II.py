4df556ca93247b90ae79c9f3f300fedb9900a4ff
eric496 <eric.mlengineer@gmail.com>
2018-07-11 14:20:30 -0400

    add 107

---
+++ array/107.binary_tree_level_order_traversal_II.py
@@ -0,0 +1,58 @@
+'''
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> list:
+        if not root:
+            return []
+        level, result = [root], [[root.val]]
+        while level:
+            cur_level = []
+            is_there_item_in_next_level = 0
+            for item in level:
+                if (item.left or item.right) and not is_there_item_in_next_level:
+                    result.append([])
+                    is_there_item_in_next_level = 1
+                if item.left:
+                    cur_level.append(item.left)
+                    result[-1].append(item.left.val)
+                if item.right:
+                    cur_level.append(item.right)
+                    result[-1].append(item.right.val)
+            level = cur_level
+        return result[::-1]
+
+# simplified but not well readable
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> list:
+        if not root:
+            return []
+        level, result = [root], []
+        while level:
+            result.append([node.val for node in level])
+            level = [node for el in level for node in (el.left, el.right) if node]
+        return result[::-1]
