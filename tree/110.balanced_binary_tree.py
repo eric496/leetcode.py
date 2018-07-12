0353a356c9118eb58f4dfefba2bd851f8b1e68f7
eric496 <eric.mlengineer@gmail.com>
2018-07-12 12:11:09 -0400

    add solution

--- tree/110.balanced_binary_tree.py
+++ tree/110.balanced_binary_tree.py
@@ -28,3 +28,21 @@ Given the following tree [1,2,2,3,3,null,null,4,4]:
 Return false.
 '''
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# recursive solution
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        return abs(self.getHeight(root.left) - self.getHeight(root.right)) < 2 and self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def getHeight(self, root):
+        if not root:
+            return 0
+        return 1 + max(self.getHeight(root.left), self.getHeight(root.right))
