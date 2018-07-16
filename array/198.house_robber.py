8e0d9cbcf12e44b7ef993d0116f02748a6d0368c
eric496 <eric.mlengineer@gmail.com>
2018-07-15 22:27:12 -0400

    add one more solution

--- array/198.house_robber.py
+++ array/198.house_robber.py
@@ -1,6 +1,5 @@
 '''
 You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
-
 Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
 
 Example 1:
@@ -9,6 +8,7 @@ Input: [1,2,3,1]
 Output: 4
 Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
              Total amount you can rob = 1 + 3 = 4.
+
 Example 2:
 
 Input: [2,7,9,3,1]
@@ -17,6 +17,7 @@ Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (m
              Total amount you can rob = 2 + 9 + 1 = 12.
 '''
 
+# O(n) time and O(n) space
 class Solution:
     def rob(self, nums: list) -> int:
         if not nums:
@@ -32,3 +33,11 @@ class Solution:
             else:
                 dp.append(dp[i-1])
         return dp[-1]
+
+# O(n) time and O(1) space
+class Solution:
+    def rob(self, nums: list) -> int:
+        first, second = 0, 0
+        for num in nums:
+            second, first = num+first, max(first,second)
+        return max(first,second)
