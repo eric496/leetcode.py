d831bcfe26659c6067aab85ce6263932f0df3d90
eric496 <eric.mlengineer@gmail.com>
2018-07-24 18:12:53 -0400

    add 303

---
+++ dynamic_programming/303.range_sum_query_immutable.py
@@ -0,0 +1,27 @@
+'''
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+
+Note:
+You may assume that the array does not change.
+There are many calls to sumRange function.
+'''
+
+class NumArray:
+    
+    def __init__(self, nums: list):
+        self.nums = nums
+        
+
+    def sumRange(self, i: int, j: int) -> int:
+        return sum(self.nums[i:j+1])
+        
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
