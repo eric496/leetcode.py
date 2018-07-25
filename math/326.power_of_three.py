c8c5086f23befd7952b74d9c89e41dbc9bce7bd5
eric496 <eric.mlengineer@gmail.com>
2018-07-25 12:28:35 -0400

    add 326

---
+++ math/326.power_of_three.py
@@ -0,0 +1,36 @@
+'''
+Given an integer, write a function to determine if it is a power of three.
+
+Example 1:
+
+Input: 27
+Output: true
+Example 2:
+
+Input: 0
+Output: false
+Example 3:
+
+Input: 9
+Output: true
+Example 4:
+
+Input: 45
+Output: false
+
+Follow up:
+Could you do it without using any loop / recursion?
+'''
+
+# recursive solution
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        return n > 0 and (n == 1 or (n%3 == 0 and self.isPowerOfThree(n//3)));
+
+# iterative solution
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n > 1:
+            while n%3 == 0:
+                n //= 3
+        return n == 1
