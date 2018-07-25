5c6def2939f215c26feb77e49df804b5ba0fb57e
eric496 <eric.mlengineer@gmail.com>
2018-07-25 17:46:50 -0400

    add 367

---
+++ math/367.valid_perfect_square.py
@@ -0,0 +1,24 @@
+'''
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Note: Do not use any built-in library function such as sqrt.
+
+Example 1:
+
+Input: 16
+Returns: True
+Example 2:
+
+Input: 14
+Returns: False
+'''
+
+# Newton's method
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        r = num
+        while r*r > num:
+            r = (r + num//r) // 2
+        return r*r == num
+
+# binary search
