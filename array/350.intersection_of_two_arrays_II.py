909d0fa0f079c78e19ce0a8fb7ddcc783a0dcd8d
eric496 <eric.mlengineer@gmail.com>
2018-07-25 17:37:55 -0400

    add 350

---
+++ array/350.intersection_of_two_arrays_II.py
@@ -0,0 +1,34 @@
+'''
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+
+Note:
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+
+Follow up:
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+'''
+
+class Solution:
+    def intersect(self, nums1: list, nums2: list) -> list:
+        result = []
+        nums1.sort()
+        nums2.sort()
+        p1 = p2 = 0
+        while p1 < len(nums1) and p2 < len(nums2):
+            if nums1[p1] == nums2[p2]:
+                result.append(nums1[p1])
+                p1 += 1
+                p2 += 1
+            elif nums1[p1] < nums2[p2]:
+                p1 += 1
+            else:
+                p2 += 1
+        return result
+
+# follow up 
