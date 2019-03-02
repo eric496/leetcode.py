d8b08c5a7b03cac60a7db3fdbc463c98d95d013e
eric496 <eric.mlengineer@gmail.com>
2019-03-02 13:06:53 -0500

    add solution 283

--- array/283.move_zeroes.py
+++ array/283.move_zeroes.py
@@ -11,3 +11,13 @@ You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 '''
 
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero = 0
+        for ix, n in enumerate(nums):
+            if n != 0:
+                nums[zero], nums[ix] = nums[ix], nums[zero]
+                zero += 1
