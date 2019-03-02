9f75a58d8801ff11000afc355368e1d7a6e90fc4
eric496 <eric.mlengineer@gmail.com>
2019-03-02 15:24:37 -0500

    add another solution 242

--- string/242.valid_anagram.py
+++ string/242.valid_anagram.py
@@ -17,6 +17,7 @@ Follow up:
 What if the inputs contain unicode characters? How would you adapt your solution to such case?
 '''
 
+# Solution 1: use dictionary
 class Solution:
     def isAnagram(self, s:str, t:str) -> bool:
         if len(s) != len(t):
@@ -26,3 +27,15 @@ class Solution:
             count[s[i]] = count[s[i]]+1 if s[i] in count else 1
             count[t[i]] = count[t[i]]-1 if t[i] in count else -1
         return not any(count.values())
+
+# Solution 2: use list
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count = [0] * 26
+        for ch1, ch2 in zip(s, t):
+            count[ord(ch1)-ord('a')] += 1 
+            count[ord(ch2)-ord('a')] -= 1
+        return not any(count)
+        
