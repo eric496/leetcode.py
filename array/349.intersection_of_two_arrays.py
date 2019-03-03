f6fc1c27ae660696b15f7bb73d3a90b527393920
eric496 <eric.mlengineer@gmail.com>
2019-03-02 21:57:14 -0500

    change solution

--- array/349.intersection_of_two_arrays.py
+++ array/349.intersection_of_two_arrays.py
@@ -27,10 +27,11 @@ class Solution:
         nums2.sort()
         p1 = p2 = 0
         while p1 < len(nums1) and p2 < len(nums2):
-            if nums1[p1] == nums2[p2] and nums1[p1] not in res:
-                res.append(nums1[p1])
-                p1 += 1
-                p2 += 1
+            if nums1[p1] == nums2[p2]:
+                if nums1[p1] not in res:
+                    res.append(nums1[p1])
+                    p1 += 1
+                    p2 += 1
             elif nums1[p1] < nums2[p2]:
                 p1 += 1
             else:
