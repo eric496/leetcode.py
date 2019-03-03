11d0a746c8eaa1bf313fc4495ab9669482fda642
eric496 <eric.mlengineer@gmail.com>
2019-03-02 21:43:30 -0500

    change solution

--- array/349.intersection_of_two_arrays.py
+++ array/349.intersection_of_two_arrays.py
@@ -12,27 +12,27 @@ The result can be in any order.
 # O(mn) time complexity
 class Solution:
     def intersection(self, nums1: list, nums2: list) -> list:
-        result = []
+        res = []
         for n in nums1:
             if n in nums2:
-                result.append(n)
+                res.append(n)
                 nums2.remove(n)
-        return list(set(result))
+        return list(set(res))
 
 # sort first, max(O(nlogn), O(mlogm)) time complexity
 class Solution:
     def intersection(self, nums1: list, nums2: list) -> list:
-        result = []
+        res = []
         nums1.sort()
         nums2.sort()
         p1 = p2 = 0
         while p1 < len(nums1) and p2 < len(nums2):
-            if nums1[p1] == nums2[p2] and nums1[p1] not in result:
-                result.append(nums1[p1])
+            if nums1[p1] == nums2[p2] and nums1[p1] not in res:
+                res.append(nums1[p1])
                 p1 += 1
                 p2 += 1
             elif nums1[p1] < nums2[p2]:
                 p1 += 1
             else:
                 p2 += 1
-        return result
+        return res
