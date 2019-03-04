5bc6e430bef87ff970f827db845554f238a5e29b
eric496 <eric.mlengineer@gmail.com>
2019-03-04 14:09:58 -0500

    add second solution

--- linked_list/160.intersection_of_two_linked_lists.py
+++ linked_list/160.intersection_of_two_linked_lists.py
@@ -24,26 +24,74 @@ Your code should preferably run in O(n) time and use only O(1) memory.
 #         self.val = x
 #         self.next = None
 
+# Solution One: keep track of the head
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        ptA, ptB = ListNode(-1), ListNode(-1)
+        ptA.next, ptB.next = headA, headB
+        cntA = cntB = 0 
+        
+        while ptA:
+            cntA += 1
+            ptA = ptA.next
+        
+        while ptB:
+            cntB += 1
+            ptB = ptB.next
+            
+        if cntA > cntB:
+            diff = cntA - cntB
+            longer, shorter = headA, headB
+        else:
+            diff = cntB - cntA
+            longer, shorter = headB, headA
+        
+        ptL, ptS = ListNode(-1), ListNode(-1)
+        ptL.next, ptS.next = longer, shorter
+        
+        while diff:
+            ptL = ptL.next
+            diff -= 1
+        
+        while ptL and ptS:
+            if ptL == ptS:
+                return ptL
+            else:
+                ptL = ptL.next
+                ptS = ptS.next
+        
+        return None
+
+# Solution Two: do not keep track of the head
 class Solution(object):
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         curA, curB = headA, headB
         lenA = lenB = 0
+
         while curA:
             curA = curA.next
             lenA += 1
         while curB:
             curB = curB.next
             lenB += 1
+        
         curA, curB = headA, headB
+        
         if lenA > lenB:
             for _ in range(lenA-lenB):
                 curA = curA.next
         else:
             for _ in range(lenB-lenA):
                 curB = curB.next
+        
         while curA and curB:
             if curA is curB:
                 return curA
             else:
                 curA, curB = curA.next, curB.next
+        
         return None
