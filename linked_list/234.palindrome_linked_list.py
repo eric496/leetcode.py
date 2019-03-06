e871db50f44864307a1b894695505f1ed0e09f84
eric496 <eric.mlengineer@gmail.com>
2019-03-06 12:29:04 -0500

    add solution

--- linked_list/234.palindrome_linked_list.py
+++ linked_list/234.palindrome_linked_list.py
@@ -20,3 +20,26 @@ Could you do it in O(n) time and O(1) space?
 #         self.val = x
 #         self.next = None
 
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        slow = fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev = None
+        
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+        
+        while prev:
+            if prev.val != head.val:
+                return False
+            prev = prev.next
+            head = head.next
+        
+        return True
