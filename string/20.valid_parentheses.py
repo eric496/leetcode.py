a33ea2f667ac957d2d82dd33f7342cd568135eae
eric496 <eric.mlengineer@gmail.com>
2019-03-18 21:19:18 -0400

    change solution

--- string/20.valid_parentheses.py
+++ string/20.valid_parentheses.py
@@ -40,11 +40,11 @@ Thought:
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
+        stk = []
         pairs = {'(':')', '{':'}', '[':']'}
         for ch in s:
             if ch in pairs:
-                stack.append(pairs[ch])
-            elif not stack or stack.pop() != ch:
+                stk.append(pairs[ch])
+            elif not stk or stk.pop() != ch:
                 return False
-        return stack == []
+        return stk == []
