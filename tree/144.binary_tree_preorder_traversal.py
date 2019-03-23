d930fddcad94629051b747a857ac4cbe2a464eb4
eric496 <eric.mlengineer@gmail.com>
2019-03-23 00:12:28 -0400

    add 144

---
+++ tree/144.binary_tree_preorder_traversal.py
@@ -0,0 +1,58 @@
+"""
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Solution 1: DFS
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        self.dfs(root, res)
+        return res
+
+    def dfs(self, root: TreeNode, res: List[int]) -> None:
+        if not root:
+            return
+
+        res.append(root.val)
+        self.dfs(root.left, res)
+        self.dfs(root.right, res)
+
+# Solution 2: Stack
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+
+        stk, res = [root], []
+
+        while stk:
+            node = stk.pop()
+            res.append(node.val)
+
+            if node.right:
+                stk.append(node.right)
+            if node.left:
+                stk.append(node.left)
+
+        return res
+
