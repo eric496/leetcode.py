bbe62a03a3299ca6854a129b3bf0bb74f4923333
eric496 <eric.mlengineer@gmail.com>
2019-03-22 22:26:11 -0400

    add 102

---
+++ tree/102.binary_tree_level_order_traversal.py
@@ -0,0 +1,71 @@
+"""
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Solution 1
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+
+        q, res = [root], []
+
+        while q:
+            level_q, level_res = [], []
+            for node in q:
+                if node.left:
+                    level_q.append(node.left)
+                if node.right:
+                    level_q.append(node.right)
+                level_res.append(node.val)
+            res.append(level_res)
+            q = level_q
+
+        return res
+
+# Solution 2
+# Improvements: 1) Use deque because popleft is O(1) time
+#               2) Count the level size so do not need extra level array
+from collections import deque
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+
+        q, res = deque([root]), []
+
+        while q:
+            level, size = [], len(q)
+            for i in range(size):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(level)
+
+        return res
+
