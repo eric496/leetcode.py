2e4c9edb8fc92741fa7d84d2e60d6ec4f607b594
eric496 <eric.mlengineer@gmail.com>
2019-03-25 14:21:09 -0400

    add 117

---
+++ tree/117.populating_next_right_pointers_in_each_node_II.py
@@ -0,0 +1,73 @@
+"""
+Given a binary tree
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+Initially, all next pointers are set to NULL.
+
+Example:
+Input: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+Output: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":null,"right":null,"val":7},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"6","left":null,"next":null,"right":{"$ref":"5"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"6"},"val":1}
+Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B.
+
+Note:
+You may only use constant extra space.
+Recursive approach is fine, implicit stack space does not count as extra space for this problem.
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+# Recursive
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        self.dfs(root)
+        return root
+    
+    def dfs(self, root: 'Node') -> None:
+        if not root:
+            return
+        
+        if not root.left and not root.right:
+            return
+        
+        if root.left:
+            if root.right:
+                root.left.next = root.right
+            else:
+                node = root.next
+                while node:
+                    if node.left:
+                        root.left.next = node.left
+                        break
+                    elif node.right:
+                        root.left.next = node.right
+                        break
+                    else:
+                        node = node.next
+                    
+        if root.right:
+            node = root.next
+            while node:
+                if node.left:
+                    root.right.next = node.left
+                    break
+                elif node.right:
+                    root.right.next = node.right
+                    break
+                else:
+                    node = node.next
+        
+        self.dfs(root.right)
+        self.dfs(root.left)
