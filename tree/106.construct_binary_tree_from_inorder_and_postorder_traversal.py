0f4acc12caaa5bbe844d9023b2b9ad9734d76039
eric496 <eric.mlengineer@gmail.com>
2019-03-25 12:16:53 -0400

    add 106

--- tree/106.construct_binary_tree_from_inorder_and_postorder_traversal.py
+++ tree/106.construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -1,12 +1,12 @@
 """
-Given preorder and inorder traversal of a tree, construct the binary tree.
+Given inorder and postorder traversal of a tree, construct the binary tree.
 
 Note:
 You may assume that duplicates do not exist in the tree.
 
 For example, given
-preorder = [3,9,20,15,7]
 inorder = [9,3,15,20,7]
+postorder = [9,15,7,20,3]
 Return the following binary tree:
 
     3
@@ -15,6 +15,7 @@ Return the following binary tree:
     /  \
    15   7
 """
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -22,14 +23,14 @@ Return the following binary tree:
 #         self.left = None
 #         self.right = None
 
-# Recursive 
+# Recursive
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
         if not inorder:
             return None
-            
-        ix = inorder.index(preorder.pop(0))
+        
+        ix = inorder.index(postorder.pop())
         root = TreeNode(inorder[ix])
-        root.left = self.buildTree(preorder, inorder[:ix])
-        root.right = self.buildTree(preorder, inorder[ix+1:])
+        root.right = self.buildTree(inorder[ix+1:], postorder)
+        root.left = self.buildTree(inorder[:ix], postorder)
         return root
