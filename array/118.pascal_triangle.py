eb5b004cbf40b5c7db9d763d0c03abec7b9df28f
eric496 <eric.mlengineer@gmail.com>
2019-03-27 11:40:25 -0400

    add recursive solution

--- array/118.pascal_triangle.py
+++ array/118.pascal_triangle.py
@@ -15,6 +15,28 @@ Output:
 ]
 '''
 
+# Recursive
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]*(n+1) for n in range(numRows)]
+        
+        if numRows < 2:
+            return res
+        
+        self.recurse(2, res)
+        
+        return res
+        
+    def recurse(self, numRows: int, res: List[List[int]]) -> None:
+        if numRows >= len(res):
+            return
+        
+        for i in range(1, len(res[numRows])-1):
+            res[numRows][i] = res[numRows-1][i-1] + res[numRows-1][i]
+        
+        self.recurse(numRows+1, res)
+
+# Iterative
 class Solution:
     def generate(self, numRows: int) -> list:
         if numRows == 0:
@@ -23,21 +45,26 @@ class Solution:
             return [[1]]
         if numRows == 2:
             return [[1],[1,1]]
+        
         triangle = [[1], [1,1]]
+        
         for _ in range(2, numRows):
             level = [1,1]
             for i in range(len(triangle[-1])-1):
                 level.insert(-1, triangle[-1][i]+triangle[-1][i+1])
             triangle.append(level)
+        
         return triangle
 
-# a more concise solution
+# Improved iterative 
 class Solution:
     def generate(self, numRows: int) -> list:
         triangle = []
+        
         for i in range(numRows):
             triangle.append([1] * (i+1))
             if i > 1:
                 for j in range(1, i):
                     triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+
         return triangle
