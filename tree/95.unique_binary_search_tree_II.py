4969ff57d0060b3ff384d81090846a647546d410
eric496 <eric.mlengineer@gmail.com>
2019-03-28 15:10:41 -0400

    add 95

---
+++ tree/95.unique_binary_search_tree_II.py
@@ -0,0 +1,52 @@
+"""
+Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+
+Example:
+Input: 3
+Output:
+[
+  [1,null,3,2],
+  [3,2,null,1],
+  [3,1,null,null,2],
+  [2,1,3],
+  [1,null,2,null,3]
+]
+Explanation:
+The above output corresponds to the 5 unique BST's shown below:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        return self.helper(1, n) if n else []
+    
+    def helper(self, start, end):
+            if start > end:
+                return [None]
+            
+            res = []
+            
+            for i in range(start, end+1):
+                left = self.helper(start, i-1)
+                right = self.helper(i+1, end)
+                
+                for l in left:
+                    for r in right:
+                        root = TreeNode(i)
+                        root.left = l
+                        root.right = r
+                        res.append(root)
+            
+            return res
