e92413b92122f2d49923e23cb08c07b8f4167850
eric496 <eric.mlengineer@gmail.com>
2019-03-29 00:49:34 -0400

    add solutions

--- array/35.search_insert_position.py
+++ array/35.search_insert_position.py
@@ -59,3 +59,18 @@ class Solution:
                 low = mid + 1
 
         return low
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums)-1
+
+        while low < high:
+            mid = low + (high-low)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return low if nums[low] >= target else low + 1
