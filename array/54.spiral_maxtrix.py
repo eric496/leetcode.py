34afd073153b9f77a9a5448932b0df8a272cef51
eric496 <eric.mlengineer@gmail.com>
2019-03-28 23:27:59 -0400

    add 54

---
+++ array/54.spiral_maxtrix.py
@@ -0,0 +1,32 @@
+"""
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+Example 1:
+Input:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Example 2:
+Input:
+[
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9,10,11,12]
+]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+"""
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+
+        while matrix:
+            res.extend(list(matrix.pop(0)))
+            matrix = list(zip(*matrix))[::-1]
+
+        return res
+
