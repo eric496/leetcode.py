e1c2b8329ada3288bfdf49cd58352a53f15190dd
eric496 <eric.mlengineer@gmail.com>
2019-03-29 17:40:19 -0400

    add 146

---
+++ design/146.lru_cache.py
@@ -0,0 +1,125 @@
+"""
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+LRUCache cache = new LRUCache( 2 /* capacity */ );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+"""
+
+# Solution 1: Does not work
+class ListNode:
+    def __init__(self, k, v):
+        self.key = k
+        self.val = v
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity 
+        self.d = {}
+        self.head = ListNode(0, 0)
+        self.tail = ListNode(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key in self.d:
+            node = self.d[key]
+            self.remove_from_list(node)
+            self.insert_to_front(node)
+            return node.val
+        else:
+            return -1
+        
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if key not in self.d:
+            node = ListNode(key, value)
+            self.insert_to_front(node)
+            self.d[key] = node
+        
+        if len(self.d) > self.capacity:
+            self.remove_from_list(self.tail.prev)
+            del self.d[self.tail.prev.key]
+        
+    
+    def remove_from_list(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+        
+    
+    def insert_to_front(self, node):
+        node.next = self.head.next
+        self.head.next = node
+        node.prev = self.head
+
+# Solution 2: Ordered Dict
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.d = OrderedDict()
+        
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key in self.d:
+            val = self.d.pop(key)
+            self.d[key] = val
+            return val
+        else:
+            return -1
+
+    
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if key in self.d:
+            self.d.pop(key)
+            self.d[key] = value
+        else:
+            self.d[key] = value
+            if len(self.d) > self.capacity:
+                self.d.popitem(last=False)
+    
