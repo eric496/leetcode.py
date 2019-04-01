00075a46545b0659c5f58c023ac47de5ab714ac2
eric496 <eric.mlengineer@gmail.com>
2019-04-01 12:14:28 -0400

    add 47

---
+++ backtracking/47.permutations_II.py
@@ -0,0 +1,36 @@
+"""
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+Example:
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+"""
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        self.backtrack(nums, [], [0]*len(nums), res)
+        
+        return res
+        
+    def backtrack(self, nums: List[int], cur_perm: List[int], used: List[bool], res: List[List[int]]) -> None:
+        if len(cur_perm) == len(nums):
+            res.append(cur_perm[:])
+        else:
+            for ix, n in enumerate(nums):
+                if used[ix]:
+                    continue
+                if ix>0 and n==nums[ix-1] and not used[ix-1]:
+                    continue
+                
+                used[ix] = 1
+                cur_perm.append(n)
+                self.backtrack(nums, cur_perm, used, res)
+                used[ix] = 0
+                cur_perm.pop()
