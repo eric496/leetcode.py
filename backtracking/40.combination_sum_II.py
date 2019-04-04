b1d89f3170a3c88d5ea2f78a6d6ea83f6f72c003
eric496 <eric.mlengineer@gmail.com>
2019-04-04 15:08:23 -0400

    add 40

---
+++ backtracking/40.combination_sum_II.py
@@ -0,0 +1,49 @@
+"""
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+Each number in candidates may only be used once in the combination.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+
+Example 1:
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+
+Example 2:
+Input: candidates = [2,5,2,1,2], target = 5,
+A solution set is:
+[
+  [1,2,2],
+  [5]
+]
+"""
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+        self.dfs(candidates, target, 0, [], res)
+        
+        return res
+        
+    def dfs(self, candidates: List[int], remain: int, start: int, comb: List[int], res: List[List[int]]) -> None:
+        if remain < 0:
+            return
+        elif remain == 0:
+            res.append(list(comb))
+            return
+        else:
+            for i in range(start, len(candidates)):
+                if i>start and candidates[i]==candidates[i-1]:
+                    continue
+                comb.append(candidates[i])
+                self.dfs(candidates, remain-candidates[i], i+1, comb, res)
+                comb.pop()
+    
