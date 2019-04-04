73cc1b2f655216f4592a009c29b66a5cd07aa0ab
eric496 <eric.mlengineer@gmail.com>
2019-04-04 18:08:48 -0400

    add 605

--- array/605.can_place_flowers.py
+++ array/605.can_place_flowers.py
@@ -16,3 +16,21 @@ The input array size is in the range of [1, 20000].
 n is a non-negative integer which won't exceed the input array size.
 '''
 
+# Trick: pad head and tail
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if not n:
+            return True
+        
+        flowerbed = [0] + flowerbed + [0]
+        
+        for i in range(1, len(flowerbed)-1):
+            if flowerbed[i-1] == flowerbed[i] == flowerbed[i+1] == 0:
+                flowerbed[i] = 1
+                n -= 1
+            
+            if not n:
+                return True
+            
+        return False
+    
