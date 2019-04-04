4689d8b72f7fcec2d5f586b32dd6a5e4e4316638
eric496 <eric.mlengineer@gmail.com>
2019-04-04 18:27:09 -0400

    add solution

--- tree/572.subtree_of_another_tree.py
+++ tree/572.subtree_of_another_tree.py
@@ -32,3 +32,32 @@ Given tree t:
 Return false.
 """
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:     
+        if not s:
+            return False
+        
+        if self.dfs(s, t):
+            return True
+        
+        if self.isSubtree(s.left, t) or self.isSubtree(s.right, t):
+            return True
+        
+        return False
+    
+    
+    def dfs(self, s: TreeNode, t: TreeNode) -> bool:
+        if not s and not t:
+            return True
+        
+        if not s or not t:
+            return False
+            
+        return s.val == t.val and self.dfs(s.left, t.left) and self.dfs(s.right, t.right)
