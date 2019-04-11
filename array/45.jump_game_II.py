8bc1ba4985bbc63d161751223e0401604864a3ce
eric496 <eric.mlengineer@gmail.com>
2019-04-11 14:59:52 -0400

    add 45

---
+++ array/45.jump_game_II.py
@@ -0,0 +1,26 @@
+"""
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Your goal is to reach the last index in the minimum number of jumps.
+
+Example:
+Input: [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2.
+    Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Note:
+You can assume that you can always reach the last index.
+"""
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        res = end = max_jumps = 0
+        
+        for ix, n in enumerate(nums[:-1]):
+            max_jumps = max(max_jumps, ix+n)
+            if ix == end:
+                res += 1
+                end = max_jumps
+        
+        return res
