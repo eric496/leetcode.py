f31f0a07ebdd94093695e2dde10119987f65691c
eric496 <eric.mlengineer@gmail.com>
2019-04-11 22:32:49 -0400

    add 55

---
+++ array/55.jump_game.py
@@ -0,0 +1,34 @@
+"""
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
+
+Example 1:
+Input: [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+Input: [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum
+             jump length is 0, which makes it impossible to reach the last index.input
+"""
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) < 2:
+            return True
+
+        max_reach = 0
+
+        for idx, n in enumerate(nums):
+            if idx <= max_reach:
+                max_reach = max(max_reach, idx+n)
+                if max_reach >= len(nums)-1:
+                    return True
+            else:
+                return False
+
+        return False
+
