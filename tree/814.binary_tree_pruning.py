b4e4221ebdab6b03628afc65efc873ba51b3f6da
eric496 <eric.mlengineer@gmail.com>
2019-04-17 16:56:39 -0400

    add 814

---
+++ tree/814.binary_tree_pruning.py
@@ -0,0 +1,29 @@
+"""
+We are given the head node root of a binary tree, where additionally every node's value is either a 0 or a 1.
+Return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+(Recall that the subtree of a node X is X, plus every node that is a descendant of X.)
+
+Example 1:
+Input: [1,null,0,0,1]
+Output: [1,null,0,null,1]
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Recursion
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        if not root: 
+            return None
+        
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        
+        if not root.left and not root.right and not root.val:
+            return None
+        
+        return root
