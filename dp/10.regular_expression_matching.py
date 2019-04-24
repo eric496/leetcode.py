a874a27a452f1761c52d4aa28290b5ae95de033c
eric496 <eric.mlengineer@gmail.com>
2019-04-24 10:44:32 -0400

    add 10

---
+++ dp/10.regular_expression_matching.py
@@ -0,0 +1,66 @@
+"""
+Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Note:
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like . or *.
+
+Example 1:
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+Input:
+s = "aa"
+p = "a*"
+Output: true
+Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+Example 3:
+Input:
+s = "ab"
+p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+
+Example 4:
+Input:
+s = "aab"
+p = "c*a*b"
+Output: true
+Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
+
+Example 5:
+Input:
+s = "mississippi"
+p = "mis*is*p*."
+Output: false
+"""
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:        
+        dp = [[False]*(len(p)+1) for _ in range(len(s)+1)]
+        dp[0][0] = True
+        
+        for i, ch in enumerate(p):
+            if ch == '*' and dp[0][i-1]:
+                dp[0][i+1] = True
+        
+        for i, sch in enumerate(s):
+            for j, pch in enumerate(p):
+                if pch == '.' or pch == sch:
+                    dp[i+1][j+1] = dp[i][j]
+                if pch == '*':
+                    if p[j-1] != sch and p[j-1] != '.':
+                        dp[i+1][j+1] = dp[i+1][j-1]
+                    else:
+                        dp[i+1][j+1] = dp[i][j+1] or dp[i+1][j-1]
+        
+        return dp[len(s)][len(p)]
+        
