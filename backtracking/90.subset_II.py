e5ee80df4037c069dcac1e2cd15c020761d1597f
eric496 <eric.mlengineer@gmail.com>
2019-04-24 23:25:41 -0400

    add 90

---
+++ backtracking/90.subset_II.py
@@ -0,0 +1,37 @@
+"""
+Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+Input: [1,2,2]
+Output:
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+"""
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        self.backtrack(nums, 0, [], res)
+
+        return res
+
+
+    def backtrack(self, nums: List[int], start: int, cur_res: List[int], res: List[List[int]]) -> None:
+        res.append(list(cur_res))
+
+        for i in range(start, len(nums)):
+            if i > start and nums[i] == nums[i-1]:
+                continue
+            cur_res.append(nums[i])
+            self.backtrack(nums, i+1, cur_res, res)
+            cur_res.pop()
+
