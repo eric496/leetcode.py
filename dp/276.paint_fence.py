9898252faeb2b8d6347b08405e13aa96496c53a6
eric496 <eric.mlengineer@gmail.com>
2019-05-08 23:40:44 -0400

    Add solution

--- dp/276.paint_fence.py
+++ dp/276.paint_fence.py
@@ -21,3 +21,17 @@ Explanation: Take c1 as color 1, c2 as color 2. All possible ways are:
    6         c2     c2     c1
 """
 
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return k
+
+        same, diff = k, k * (k-1)
+
+        for i in range(3, n+1):
+            same, diff = diff, (same+diff) * (k-1)
+
+        return same + diff
+
