e99da7ff0b1b1855b614b993e00502da65caf01d
eric496 <eric.mlengineer@gmail.com>
2019-05-13 15:05:55 -0400

    Add 84

---
+++ stack/84.largest_rectangle_in_histogram.py
@@ -0,0 +1,8 @@
+"""
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+
+Example:
+Input: [2,1,5,6,2,3]
+Output: 10
+"""
+
