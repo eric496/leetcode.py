68cd82e7b966a71c4a62b23f1fec76a8613fa092
eric496 <eric.mlengineer@gmail.com>
2019-05-15 11:21:13 -0400

    Add solution

--- dp/300.longest_increasing_subsequence.py
+++ dp/300.longest_increasing_subsequence.py
@@ -13,3 +13,21 @@ Your algorithm should run in O(n2) complexity.
 Follow up: Could you improve it to O(n log n) time complexity?
 """
 
+# Solution 1: DP
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        dp = [1] * len(nums)
+        max_len = 1
+        
+        for i, n in enumerate(nums):
+            for j, m in enumerate(nums[:i]):
+                if m < n:
+                    dp[i] = max(dp[i], dp[j]+1)
+                    max_len = max(max_len, dp[i])
+                    
+        return max_len
+
+# Solution 2: Binary Search
