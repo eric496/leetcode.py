fbb5c36bef6d4245bf89746e5536683c245ac42e
eric496 <eric.mlengineer@gmail.com>
2019-05-20 18:27:09 -0400

    Add 287

---
+++ binary_search/287.find_the_duplicate_number.py
@@ -0,0 +1,62 @@
+"""
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+Example 1:
+Input: [1,3,4,2,2]
+Output: 2
+
+Example 2:
+Input: [3,1,3,4,2]
+Output: 3
+
+Note:
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
+"""
+
+# Pigeonhole Principle
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        low, high = 1, len(nums)-1
+        
+        while low < high:
+            mid = low + (high-low)//2
+            cnt = 0
+            
+            for n in nums:
+                if n <= mid:
+                   cnt += 1
+                
+            if cnt <= mid:
+                low = mid + 1
+            else:
+                high = mid
+                
+        return low
+
+# Floyd's Cycle Finding Algorithm (Floyd's Tortoise and Hare Algorithm)
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        slow = fast = nums[0]
+        fast = nums[fast]
+        
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+            fast = nums[fast]
+        
+        slow = 0
+        
+        while nums[slow] != nums[fast]:
+            slow = nums[slow]
+            fast = nums[fast]
+        
+        return nums[slow]
