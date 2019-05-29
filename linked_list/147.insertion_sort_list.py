c9129c1d3807cbd9697e9a2dc7327e3dfeeb0ca9
eric496 <eric.mlengineer@gmail.com>
2019-05-29 18:55:49 -0400

    Modify solution

--- linked_list/147.insertion_sort_list.py
+++ linked_list/147.insertion_sort_list.py
@@ -16,14 +16,15 @@ Output: -1->0->3->4->5
 """
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
 
 class Solution:
     def insertionSortList(self, head: ListNode) -> ListNode:
-        sentinel = ListNode(-1)
+        sentinel = ListNode(None)
         prev = sentinel
         
         while head:
