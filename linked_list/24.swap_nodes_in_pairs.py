651086546358e62be29eeb128d2ec303a05e911f
eric496 <eric.mlengineer@gmail.com>
2019-05-29 16:37:46 -0400

    Modify solution

--- linked_list/24.swap_nodes_in_pairs.py
+++ linked_list/24.swap_nodes_in_pairs.py
@@ -6,25 +6,30 @@ Example:
 Given 1->2->3->4, you should return the list as 2->1->4->3.
 """
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+"""
+Thought process:
+    Draw a graph and walk it over. 
+"""
 
-# Iterative
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
-        sentinel = ListNode(-1)
+        sentinel = ListNode(None)
         sentinel.next = head
         walk = sentinel
         
         while walk.next and walk.next.next:
             first = walk.next
             second = walk.next.next
-            walk.next = second
             first.next = second.next
             second.next = first
+            walk.next = second
             walk = walk.next.next
             
         return sentinel.next
