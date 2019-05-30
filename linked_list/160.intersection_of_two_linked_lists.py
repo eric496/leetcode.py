64b1e2fdad0d7c2d2416194b1fa7467455a8d7de
eric496 <eric.mlengineer@gmail.com>
2019-05-30 12:30:47 -0400

    Add thought process

--- linked_list/160.intersection_of_two_linked_lists.py
+++ linked_list/160.intersection_of_two_linked_lists.py
@@ -2,14 +2,12 @@
 Write a program to find the node at which the intersection of two singly linked lists begins.
 
 For example, the following two linked lists:
-
 A:          a1 → a2
                    ↘
                      c1 → c2 → c3
                    ↗            
 B:     b1 → b2 → b3
 begin to intersect at node c1.
-
 
 Notes:
 If the two linked lists have no intersection at all, return null.
@@ -18,80 +16,52 @@ You may assume there are no cycles anywhere in the entire linked structure.
 Your code should preferably run in O(n) time and use only O(1) memory.
 '''
 
+'''
+Thought process:
+    1. If the two linked lists have intersection, then the intersection occurs from some node to the end of both linked lists.
+    2. What we want is to let both linked lists to reach their tails simultaneously so we can check whether they have intersections in the latter parts of the lists.
+    3. In order to achieve this, count the length of both lists, and let the longer one traverse the difference in the length number of nodes, which assures they will reach the end simultaneously.
+    4. From there, let both lists traverse simultaneously. If there is same node, that is where the intersection occurs.   
+'''
+
+
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 
-# Solution One: keep track of the head
+
 class Solution(object):
-    def getIntersectionNode(self, headA, headB):
-        """
-        :type head1, head1: ListNode
-        :rtype: ListNode
-        """
-        ptA, ptB = ListNode(-1), ListNode(-1)
-        ptA.next, ptB.next = headA, headB
-        cntA = cntB = 0 
+    def getIntersectionNode(self, h1, h2):
+        walk1, walk2 = h1, h2
+        len1 = len2 = 0
         
-        while ptA:
-            cntA += 1
-            ptA = ptA.next
+        while walk1:
+            walk1 = walk1.next
+            len1 += 1
         
-        while ptB:
-            cntB += 1
-            ptB = ptB.next
+        while walk2:
+            walk2 = walk2.next
+            len2 += 1
             
-        if cntA > cntB:
-            diff = cntA - cntB
-            longer, shorter = headA, headB
-        else:
-            diff = cntB - cntA
-            longer, shorter = headB, headA
+        diff = abs(len1-len2)
+        walk1, walk2 = h1, h2
+ 
+        if len1 > len2:
+            while diff:
+                walk1 = walk1.next
+                diff -= 1
+                
+        if len2 > len1:
+            while diff:
+                walk2 = walk2.next
+                diff -= 1
         
-        ptL, ptS = ListNode(-1), ListNode(-1)
-        ptL.next, ptS.next = longer, shorter
-        
-        while diff:
-            ptL = ptL.next
-            diff -= 1
-        
-        while ptL and ptS:
-            if ptL == ptS:
-                return ptL
+        while walk1 and walk2:
+            if walk1 is walk2:
+                return walk1
             else:
-                ptL = ptL.next
-                ptS = ptS.next
-        
-        return None
-
-# Solution Two: do not keep track of the head
-class Solution(object):
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        curA, curB = headA, headB
-        lenA = lenB = 0
-
-        while curA:
-            curA = curA.next
-            lenA += 1
-        while curB:
-            curB = curB.next
-            lenB += 1
-        
-        curA, curB = headA, headB
-        
-        if lenA > lenB:
-            for _ in range(lenA-lenB):
-                curA = curA.next
-        else:
-            for _ in range(lenB-lenA):
-                curB = curB.next
-        
-        while curA and curB:
-            if curA is curB:
-                return curA
-            else:
-                curA, curB = curA.next, curB.next
+                walk1, walk2 = walk1.next, walk2.next
         
         return None
