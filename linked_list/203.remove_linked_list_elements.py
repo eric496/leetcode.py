8cb5e2fdcaac13444afe815313daed836a73472a
eric496 <eric.mlengineer@gmail.com>
2019-05-30 18:43:06 -0400

    Add thought process

--- linked_list/203.remove_linked_list_elements.py
+++ linked_list/203.remove_linked_list_elements.py
@@ -2,25 +2,32 @@
 Remove all elements from a linked list of integers that have value val.
 
 Example:
-
 Input:  1->2->6->3->4->5->6, val = 6
 Output: 1->2->3->4->5
 '''
 
+'''
+Thought process:
+    Loop through the list. If the next node value equals to the target value, skip it. 
+    Otherwise, move the walking pointer by one node forward.
+'''
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution:
-    def removeElements(self, head: ListNode, val: int) -> ListNode:
-        sentinel = pt = ListNode(-1)
-        pt.next = head
-        while pt and pt.next:
-            if pt.next.val == val:
-                pt.next = pt.next.next
+    def removeElements(self, head: ListNode, val: int) -> ListNode:        
+        sentinel = walk = ListNode(None)
+        sentinel.next = head
+        
+        while walk.next:
+            if walk.next.val == val:
+                walk.next = walk.next.next
             else:
-                pt = pt.next
-                
+                walk = walk.next
+        
         return sentinel.next
