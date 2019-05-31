076e7d71ab99a5b3c93ba23b55e4bca260e74702
eric496 <eric.mlengineer@gmail.com>
2019-05-31 13:44:38 -0400

    Add thought process

--- linked_list/82.remove_duplicates_from_sorted_list_II.py
+++ linked_list/82.remove_duplicates_from_sorted_list_II.py
@@ -10,29 +10,37 @@ Input: 1->1->1->2->3
 Output: 2->3
 """
 
+"""
+Thought process:
+    1. Use a pointer to track the previous node of the current node.
+    2. Traverse the linked list, there are 2 possible conditions:
+            1) The node value is unique, then the inner walking pointer does not move. We just move prev and walk pointers one step forward.
+            2) There are duplicate values, and the inner walking pointer will skip all the duplicate values and only keep the first node with duplicate value. 
+               In thi case, we need to prev pointer's next to point to walking pointer's next so that the first node with duplicate value will be skpped as well.
+"""
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
-        sentinel = ListNode(None)
+        sentinel = walk = ListNode(None)
         sentinel.next = head
         prev = sentinel
-        walk = head
-
+        
         while walk:
+            # Don't forget to check walk.next
             while walk.next and walk.val == walk.next.val:
                 walk = walk.next
-
-            if prev.next == walk:
+            
+            if prev.next is walk:
                 prev = prev.next
             else:
                 prev.next = walk.next
-
+                
             walk = walk.next
-
+        
         return sentinel.next
-
