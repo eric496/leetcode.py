854586e91c5a49b47d69b15c4271ba1c0186c581
eric496 <eric.mlengineer@gmail.com>
2019-06-24 11:36:15 -0400

    Modify solution

--- dp/1025.divisor_game.py
+++ dp/1025.divisor_game.py
@@ -26,7 +26,7 @@ class Solution:
         dp = [False] * (N+1)
         
         for i in range(2, N+1):
-            for j in range(1, i):
+            for j in range(1, i//2+1):
                 if i%j == 0 and not dp[i-j]:
                     dp[i] = True
                     break
