be1780e0b5830fc02751ee292a52142427e1e535
eric496 <eric.mlengineer@gmail.com>
2019-06-26 18:37:16 -0400

    Add 566

--- array/406.queue_reconstruction_by_height.py
+++ array/406.queue_reconstruction_by_height.py
@@ -11,6 +11,7 @@ Output:
 [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
 """
 
+
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         people.sort(key=lambda x: (-x[0], x[1]))
