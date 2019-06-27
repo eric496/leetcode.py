af617d644dadb1a9f7bede80959085f2e39ae092
eric496 <eric.mlengineer@gmail.com>
2019-06-27 15:21:20 -0400

    Add 223

---
+++ math/223.rectangle_area.py
@@ -0,0 +1,33 @@
+"""
+Find the total area covered by two rectilinear rectangles in a 2D plane.
+Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+
+Example:
+Input: A = -3, B = 0, C = 3, D = 4, E = 0, F = -1, G = 9, H = 2
+Output: 45
+
+Note:
+Assume that the total area is never beyond the maximum possible value of int.
+"""
+
+"""
+Thought process:
+    Add up the areas of two rectangles and subtract the overlapping area.
+"""
+
+
+class Solution:
+    def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> int:
+        rec1, rec2 = (C-A) * (D-B), (G-E) * (H-F)
+        
+        left = max(A, E)
+        right = min(C, G)
+        bottom = max(B, F)
+        top = min(D, H)
+        
+        if left < right and bottom < top:
+            overlap = (right-left) * (top-bottom)
+        else:
+            overlap = 0
+            
+        return rec1 + rec2 - overlap
