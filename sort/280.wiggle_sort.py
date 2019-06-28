ffa5cce0013ce56d66c550888ba153bb6b141aa5
eric496 <eric.mlengineer@gmail.com>
2019-06-28 10:59:05 -0400

    add 280

---
+++ sort/280.wiggle_sort.py
@@ -0,0 +1,20 @@
+"""
+Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+
+Example:
+Input: nums = [3,5,2,1,6,4]
+Output: One possible answer is [3,5,1,6,2,4]
+"""
+
+
+# Greedy
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        for i in range(len(nums)):
+            if i%2:
+                if nums[i] < nums[i-1]:
+                    nums[i-1], nums[i] = nums[i], nums[i-1]
+            else:
+                if i and nums[i] > nums[i-1]:
+                    nums[i-1], nums[i] = nums[i], nums[i-1]
+        
