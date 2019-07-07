f0fbc843d4ac2fd97f836f7d6b93c968a42f2ef6
eric496 <eric.mlengineer@gmail.com>
2019-07-07 15:24:37 -0400

    Add 637

---
+++ tree/637.average_of_levels_in_binary_tree.py
@@ -0,0 +1,48 @@
+"""
+Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+
+Example 1:
+Input:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Output: [3, 14.5, 11]
+Explanation:
+The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+
+Note:
+The range of node's value is in the range of 32-bit signed integer.
+"""
+
+
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import deque
+
+class Solution:
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        if not root:
+            return []
+
+        q, res = deque([root]), []
+
+        while q:
+            size, total = len(q), 0
+            for _ in range(size):
+                node = q.popleft()
+                total += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            res.append(total/size)
+
+        return res
+
