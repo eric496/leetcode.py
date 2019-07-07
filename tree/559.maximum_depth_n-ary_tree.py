9d5a29895a23ce7e5ee2b2cf8b623fec01abec11
eric496 <eric.mlengineer@gmail.com>
2019-07-06 23:16:23 -0400

    Add 559

---
+++ tree/559.maximum_depth_n-ary_tree.py
@@ -0,0 +1,37 @@
+"""
+Given a n-ary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note:
+The depth of the tree is at most 1000.
+The total number of nodes is at most 5000.
+"""
+
+"""
+class Node:
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+
+from collections import deque
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        
+        q, depth = deque([root]), 0
+        
+        while q:
+            size = len(q)
+            
+            for _ in range(size):
+                node = q.popleft()
+                for child in node.children:
+                    q.append(child)
+                
+            depth += 1
+            
+        return depth
+
