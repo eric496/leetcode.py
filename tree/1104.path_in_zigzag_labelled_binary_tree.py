2b9954ba68338759db4dbba364f7a6b67dc64dce
eric496 <eric.mlengineer@gmail.com>
2019-07-09 13:05:02 -0400

    Add 1104

---
+++ tree/1104.path_in_zigzag_labelled_binary_tree.py
@@ -0,0 +1,37 @@
+"""
+In an infinite binary tree where every node has two children, the nodes are labelled in row order.
+In the odd numbered rows (ie., the first, third, fifth,...), the labelling is left to right, while in the even numbered rows (second, fourth, sixth,...), the labelling is right to left.
+
+Given the label of a node in this tree, return the labels in the path from the root of the tree to the node with that label.
+
+Example 1:
+Input: label = 14
+Output: [1,3,4,14]
+
+Example 2:
+Input: label = 26
+Output: [1,2,6,10,26]
+ 
+Constraints:
+1 <= label <= 10^6
+"""
+
+
+class Solution:
+    def pathInZigZagTree(self, label: int) -> List[int]:
+        res = []
+        cnt = level = 1
+        
+        while label >= cnt * 2:
+            cnt *= 2
+            level += 1
+        
+        while label:
+            res.append(label)
+            level_max = 2**level - 1
+            level_min = 2 ** (level-1)
+            label = (level_max+level_min-label) // 2
+            level -= 1
+        
+        return res[::-1]
+
