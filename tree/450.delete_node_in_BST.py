351bc72c8ab459c4e2b09c8c16d83b527c38168e
eric496 <eric.mlengineer@gmail.com>
2019-07-15 16:25:27 -0400

    Add solution

--- tree/450.delete_node_in_BST.py
+++ tree/450.delete_node_in_BST.py
@@ -30,3 +30,39 @@ Another valid answer is [5,2,6,null,4,null,7].
 """
 
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return root
+        
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            
+            node = self.find_min(root.right)
+            root.val = node.val
+            root.right = self.deleteNode(root.right, root.val)
+        
+        return root
+    
+    
+    def find_min(self, node: TreeNode) -> TreeNode:
+        while node.left:
+            node = node.left
+        
+        return node
+    
