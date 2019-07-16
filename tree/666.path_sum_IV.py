8141a54e4b90c203c2764370e2014d2ff90e2bb8
eric496 <eric.mlengineer@gmail.com>
2019-07-16 11:28:59 -0400

    Add 666

---
+++ tree/666.path_sum_IV.py
@@ -0,0 +1,56 @@
+"""
+If the depth of a tree is smaller than 5, then this tree can be represented by a list of three-digits integers.
+For each integer in this list:
+The hundreds digit represents the depth D of this node, 1 <= D <= 4.
+The tens digit represents the position P of this node in the level it belongs to, 1 <= P <= 8. The position is the same as that in a full binary tree.
+The units digit represents the value V of this node, 0 <= V <= 9.
+Given a list of ascending three-digits integers representing a binary tree with the depth smaller than 5, you need to return the sum of all paths from the root towards the leaves.
+
+Example 1:
+Input: [113, 215, 221]
+Output: 12
+Explanation: 
+The tree that the list represents is:
+    3
+   / \
+  5   1
+The path sum is (3 + 5) + (3 + 1) = 12.
+
+Example 2:
+Input: [113, 221]
+Output: 4
+Explanation: 
+The tree that the list represents is: 
+    3
+     \
+      1
+The path sum is (3 + 1) = 4.
+"""
+
+
+class Solution:
+    
+    total = 0
+    
+    def pathSum(self, nums: List[int]) -> int:
+        tree = {num//10: num%10 for num in nums}
+        self.dfs(nums[0]//10, 0, tree)
+        
+        return self.total
+        
+        
+    def dfs(self, node: int, pre_sum: int, tree: dict) -> None:
+        level, pos = divmod(node, 10)
+        left = (level+1)*10 + (pos*2-1)
+        right = (level+1)*10 + (pos*2)
+        cur_sum = pre_sum + tree[node]
+        
+        if left not in tree and right not in tree:
+            self.total += cur_sum
+            return
+        
+        if left in tree:
+            self.dfs(left, cur_sum, tree)
+        
+        if right in tree:
+            self.dfs(right, cur_sum, tree)
