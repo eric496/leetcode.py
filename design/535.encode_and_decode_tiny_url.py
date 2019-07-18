b8b72f771ba5945a20220813f224d82c39176dea
eric496 <eric.mlengineer@gmail.com>
2019-07-18 14:27:45 -0400

    Add 535

---
+++ design/535.encode_and_decode_tiny_url.py
@@ -0,0 +1,43 @@
+"""
+Note: This is a companion problem to the System Design problem: Design TinyURL.
+
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+"""
+
+
+import string, random
+
+class Codec:
+    charset = string.ascii_letters + '0123456789'
+    base = 'http://tinyurl.com/'
+    code_len = 6
+    
+    def __init__(self):
+        self.url2code = {}
+        self.code2url = {}
+
+
+    def encode(self, longUrl):
+        """Encodes a URL to a shortened URL.
+        
+        :type longUrl: str
+        :rtype: str
+        """
+        while longUrl not in self.url2code:
+            code = ''.join(random.choice(self.charset) for _ in range(self.code_len))
+            if code not in self.code2url:
+                self.url2code[longUrl] = self.base + code
+                self.code2url[self.base+code] = longUrl
+        
+        return self.url2code[longUrl]
+        
+
+    def decode(self, shortUrl):
+        """Decodes a shortened URL to its original URL.
+        
+        :type shortUrl: str
+        :rtype: str
+        """
+        return self.code2url.get(shortUrl, '')
+        
