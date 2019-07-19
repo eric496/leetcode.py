f20d57d5022ba896abaf10bea2c7c4861b4ace3e
eric496 <eric.mlengineer@gmail.com>
2019-07-18 22:30:01 -0400

    Add 703

---
+++ heap/703.kth_largest_element_in_a_stream.py
@@ -0,0 +1,46 @@
+"""
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+Your KthLargest class will have a constructor which accepts an integer k and an integer array nums, which contains initial elements from the stream. For each call to the method KthLargest.add, return the element representing the kth largest element in the stream.
+
+Example:
+int k = 3;
+int[] arr = [4,5,8,2];
+KthLargest kthLargest = new KthLargest(3, arr);
+kthLargest.add(3);   // returns 4
+kthLargest.add(5);   // returns 5
+kthLargest.add(10);  // returns 5
+kthLargest.add(9);   // returns 8
+kthLargest.add(4);   // returns 8
+
+Note:
+You may assume that nums' length ≥ k-1 and k ≥ 1.
+"""
+
+
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = []
+
+        while nums:
+            if len(self.heap) < k:
+                heapq.heappush(self.heap, nums.pop())
+            elif nums[-1] > self.heap[0]:
+                heapq.heappop(self.heap)
+                heapq.heappush(self.heap, nums.pop())
+            else:
+                nums.pop()
+
+
+    def add(self, val: int) -> int:
+        if len(self.heap) < self.k:
+            heapq.heappush(self.heap, val)
+        elif val > self.heap[0]:
+            heapq.heappop(self.heap)
+            heapq.heappush(self.heap, val)
+
+        return self.heap[0]
+
