f66d99115b8bfdbab402199cb1d5f9b79589cdb0
eric496 <eric.mlengineer@gmail.com>
2019-07-24 10:50:35 -0400

    Add 894

--- tree/894.all_possible_full_binary_trees.py
+++ tree/894.all_possible_full_binary_trees.py
@@ -4,12 +4,38 @@ Return a list of all possible full binary trees with N nodes.  Each element of t
 Each node of each tree in the answer must have node.val = 0.
 You may return the final list of trees in any order.
 
-Example 1:
-Input: 7
-Output: [[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]
-
 Note:
 1 <= N <= 20
 """
 
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    
+    memo = {1: [TreeNode(0)]}
+    
+    def allPossibleFBT(self, N: int) -> List[TreeNode]:
+        if N in self.memo:
+            return self.memo[N]
+        
+        res = []
+        
+        for i in range(1, N-1, 2):
+            for left in self.allPossibleFBT(i):
+                for right in self.allPossibleFBT(N-i-1):
+                    root = TreeNode(0)
+                    root.left = left
+                    root.right = right
+                    res += [root]
+        
+        self.memo[N] = res
+        
+        return res
+        
