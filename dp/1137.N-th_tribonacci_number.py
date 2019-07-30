604b011c4f9f1b3d3d772cdbaf10b86a5a3b0fbf
eric496 <eric.mlengineer@gmail.com>
2019-07-30 18:14:48 -0400

    Add 1137

---
+++ dp/1137.N-th_tribonacci_number.py
@@ -0,0 +1,38 @@
+"""
+The Tribonacci sequence Tn is defined as follows: 
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+Given n, return the value of Tn.
+
+Example 1:
+Input: n = 4
+Output: 4
+Explanation:
+T_3 = 0 + 1 + 1 = 2
+T_4 = 1 + 1 + 2 = 4
+
+Example 2:
+Input: n = 25
+Output: 1389537
+
+Constraints:
+0 <= n <= 37
+The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+"""
+
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        first, second, third = 0, 1, 1
+        
+        if n == 0:
+            return first
+        elif n == 1:
+            return second
+        elif n == 2:
+            return third
+        else:
+            for i in range(3, n+1):
+                first, second, third = second, third, first + second + third
+        
+        return third
+        
