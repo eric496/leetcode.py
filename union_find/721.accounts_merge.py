2694180f20e048cfb125c5fb8bc7a8f4ba683197
eric496 <eric.mlengineer@gmail.com>
2019-08-05 15:11:29 -0400

    Add 721

---
+++ union_find/721.accounts_merge.py
@@ -0,0 +1,56 @@
+"""
+Given a list accounts, each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
+Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some email that is common to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
+After merging the accounts, return the accounts in the following format: the first element of each account is the name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
+
+Example 1:
+Input: 
+accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]
+Output: [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],  ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
+Explanation: 
+The first and third John's are the same person as they have the common email "johnsmith@mail.com".
+The second John and Mary are different people as none of their email addresses are used by other accounts.
+We could return these lists in any order, for example the answer [['Mary', 'mary@mail.com'], ['John', 'johnnybravo@mail.com'], 
+['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would still be accepted.
+
+Note:
+The length of accounts will be in the range [1, 1000].
+The length of accounts[i] will be in the range [1, 10].
+The length of accounts[i][j] will be in the range [1, 30].
+"""
+
+
+from collections import defaultdict
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        parent = {}
+        email_to_name = {}
+        
+        for account in accounts:
+            name, *emails = account
+            
+            for email in emails:
+                if email not in parent:
+                    parent[email] = email
+                email_to_name[email] = name
+                
+                self.union(email, emails[0], parent)
+                
+        trees = defaultdict(list)
+        
+        for email in parent:
+            trees[self.find(email, parent)].append(email)
+            
+        return [[email_to_name[root]] + sorted(emails) for (root, emails) in trees.items()]
+    
+        
+    def find(self, email: str, parent: dict) -> str:
+        if parent[email] != email:
+            parent[email] = self.find(parent[email], parent)
+        
+        return parent[email]
+    
+    
+    def union(self, email1: str, email2: str, parent: dict) -> None:
+        parent[self.find(email1, parent)] = self.find(email2, parent)
