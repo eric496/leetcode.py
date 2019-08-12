99bc1e3b786c41f6ee5abd2347060d0e8335108c
eric496 <eric.mlengineer@gmail.com>
2019-08-12 12:23:48 -0400

    Add 1154

---
+++ math/1154.day_of_the_year.py
@@ -0,0 +1,56 @@
+"""
+Given a string date representing a Gregorian calendar date formatted as YYYY-MM-DD, return the day number of the year.
+
+Example 1:
+Input: date = "2019-01-09"
+Output: 9
+Explanation: Given date is the 9th day of the year in 2019.
+
+Example 2:
+Input: date = "2019-02-10"
+Output: 41
+
+Example 3:
+Input: date = "2003-03-01"
+Output: 60
+
+Example 4:
+Input: date = "2004-03-01"
+Output: 61
+
+Constraints:
+date.length == 10
+date[4] == date[7] == '-', and all other date[i]'s are digits
+date represents a calendar date between Jan 1st, 1900 and Dec 31, 2019.
+"""
+
+
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        year, month, day = map(int, date.split('-'))
+        
+        days_in_month = {
+            1: 31, 
+            2: 28, 
+            3: 31,
+            4: 30,
+            5: 31,
+            6: 30,
+            7: 31, 
+            8: 31,
+            9: 30,
+            10: 31,
+            11: 30,
+            12: 31
+        }
+        
+        res = day
+        
+        for m in range(1, month):
+            res += days_in_month[m]
+            
+        if not year%4 and year%100 or not year%100 and not year%400:
+            res += 1 if month > 2 else 0
+            
+        return res
+    
