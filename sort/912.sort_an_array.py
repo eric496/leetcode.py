d8d9d7b3f172de59fc6babf739a95cfc7d5b8bc0
eric496 <eric.mlengineer@gmail.com>
2019-08-21 17:32:15 -0400

    Add merge sort solution

--- sort/912.sort_an_array.py
+++ sort/912.sort_an_array.py
@@ -14,3 +14,40 @@ Note:
 -50000 <= A[i] <= 50000
 """
 
+
+# Merge sort
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) > 1:
+            mid = len(nums) // 2
+            left = nums[:mid]
+            right = nums[mid:]
+            
+            self.sortArray(left)
+            self.sortArray(right)
+            
+            i = j = k = 0
+            
+            while i < len(left) and j < len(right):
+                if left[i] < right[j]:
+                    nums[k] = left[i]
+                    i += 1
+                else:
+                    nums[k] = right[j]
+                    j += 1
+                k += 1
+                
+            while i < len(left):
+                nums[k] = left[i]
+                i += 1
+                k += 1
+            
+            while j < len(right):
+                nums[k] = right[j]
+                j += 1
+                k += 1
+            
+        return nums
+
+
+# Quick sort
