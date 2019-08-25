fe6697c094b2fdf6d6783085c652eb27b8b4015f
eric496 <eric.mlengineer@gmail.com>
2019-08-25 14:12:49 -0400

    Add 1161

---
+++ tree/1161.maximum_level_sum_of_a_binary_tree.py
@@ -0,0 +1,59 @@
+"""
+Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+Return the smallest level X such that the sum of all the values of nodes at level X is maximal.
+
+Example 1:
+Input: [1,7,0,7,-8,null,null]
+Output: 2
+Explanation:
+Level 1 sum = 1.
+Level 2 sum = 7 + 0 = 7.
+Level 3 sum = 7 + -8 = -1.
+So we return the level with the maximum sum which is level 2.
+
+Note:
+The number of nodes in the given tree is between 1 and 10^4.
+-10^5 <= node.val <= 10^5
+"""
+
+
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def maxLevelSum(self, root: TreeNode) -> int:
+        q = deque([root])
+        level_sums = []
+
+        while q:
+            size = len(q)
+            level = []
+
+            for _ in range(size):
+                node = q.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+
+                if node.right:
+                    q.append(node.right)
+
+            level_sums.append(sum(level))
+
+        max_level, max_sum = -1, float('-inf')
+
+        for level,level_sum in enumerate(level_sums, 1):
+            if level_sum > max_sum:
+                max_sum = level_sum
+                max_level = level
+
+        return max_level
+
