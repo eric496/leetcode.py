35bc1aa07f0e2207cfe82fbfd6dd544136166e55
eric496 <eric.mlengineer@gmail.com>
2019-09-25 11:38:24 -0400

    Add 137

---
+++ bit_manipulation/137.single_number_II.py
@@ -0,0 +1,26 @@
+"""
+Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+Input: [2,2,3,2]
+Output: 3
+
+Example 2:
+Input: [0,1,0,1,0,1,99]
+Output: 99
+"""
+
+
+# Detailed explanation please see https://leetcode.com/problems/single-number-ii/discuss/?currentPage=1&orderBy=most_votes&query=
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ones = twos = 0
+        
+        for num in nums:
+            ones = (ones^num) & ~twos
+            twos = (twos^num) & ~ones
+        
+        return ones
