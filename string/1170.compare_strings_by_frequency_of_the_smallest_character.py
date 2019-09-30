c76e06999fd2c3ae4c6b16bd7fb12feeb97ea97f
eric496 <eric.mlengineer@gmail.com>
2019-09-30 12:31:49 -0400

    Add 1190

---
+++ string/1170.compare_strings_by_frequency_of_the_smallest_character.py
@@ -0,0 +1,42 @@
+"""
+Let's define a function f(s) over a non-empty string s, which calculates the frequency of the smallest character in s. For example, if s = "dcce" then f(s) = 2 because the smallest character is "c" and its frequency is 2.
+Now, given string arrays queries and words, return an integer array answer, where each answer[i] is the number of words such that f(queries[i]) < f(W), where W is a word in words.
+
+Example 1:
+Input: queries = ["cbd"], words = ["zaaaz"]
+Output: [1]
+Explanation: On the first query we have f("cbd") = 1, f("zaaaz") = 3 so f("cbd") < f("zaaaz").
+
+Example 2:
+Input: queries = ["bbb","cc"], words = ["a","aa","aaa","aaaa"]
+Output: [1,2]
+Explanation: On the first query only f("bbb") < f("aaaa"). On the second query both f("aaa") and f("aaaa") are both > f("cc").
+ 
+Constraints:
+1 <= queries.length <= 2000
+1 <= words.length <= 2000
+1 <= queries[i].length, words[i].length <= 10
+queries[i][j], words[i][j] are English lowercase letters.
+"""
+
+
+class Solution:
+    def numSmallerByFrequency(self, queries: List[str], words: List[str]) -> List[int]:
+        q_cnt = [self.f(q) for q in queries]
+        w_cnt = [self.f(w) for w in words]
+        
+        return [sum(q < w for w in w_cnt) for q in q_cnt]
+        
+        
+    def f(self, s: str) -> int:
+        min_c, cnt = chr(ord('z')+1), 1
+        
+        for c in s:
+            if ord(c) < ord(min_c):
+                min_c = c
+                cnt = 1
+            elif c == min_c:
+                cnt += 1
+        
+        return cnt
+        
