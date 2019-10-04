e217f977323c26c2932ec66e837391c1390cb856
eric496 <eric.mlengineer@gmail.com>
2019-10-04 17:57:42 -0400

    Add 531

---
+++ array/531.lonely_pixel_I.py
@@ -0,0 +1,37 @@
+"""
+Given a picture consisting of black and white pixels, find the number of black lonely pixels.
+The picture is represented by a 2D char array consisting of 'B' and 'W', which means black and white pixels respectively.
+A black lonely pixel is character 'B' that located at a specific position where the same row and same column don't have any other black pixels.
+
+Example:
+Input: 
+[['W', 'W', 'B'],
+ ['W', 'B', 'W'],
+ ['B', 'W', 'W']]
+Output: 3
+Explanation: All the three 'B's are black lonely pixels.
+
+Note:
+The range of width and height of the input 2D array is [1,500].
+"""
+
+
+class Solution:
+    def findLonelyPixel(self, picture: List[List[str]]) -> int:
+        m, n = len(picture), len(picture[0])
+        row_cnt, col_cnt = [0]*m, [0]*n
+        
+        for i in range(m):
+            for j in range(n):
+                if picture[i][j] == 'B':
+                    row_cnt[i] += 1
+                    col_cnt[j] += 1
+        
+        cnt = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if picture[i][j] == 'B' and row_cnt[i]==1 and col_cnt[j]==1:
+                    cnt += 1
+        
+        return cnt
