c71cca4671a45f2b43e737d511dbe4e79256abb6
eric496 <eric.mlengineer@gmail.com>
2019-11-11 16:21:11 -0500

    Add 1167

---
+++ greedy/1167.minimum_cost_to_connect_sticks.py
@@ -0,0 +1,33 @@
+"""
+You have some sticks with positive integer lengths.
+You can connect any two sticks of lengths X and Y into one stick by paying a cost of X + Y.  You perform this action until there is one stick remaining.
+Return the minimum cost of connecting all the given sticks into one stick in this way.
+
+Example 1:
+Input: sticks = [2,4,3]
+Output: 14
+
+Example 2:
+Input: sticks = [1,8,3,5]
+Output: 30
+
+Constraints:
+1 <= sticks.length <= 10^4
+1 <= sticks[i] <= 10^4
+"""
+
+
+import heapq
+
+class Solution:
+    def connectSticks(self, sticks: List[int]) -> int:
+        heapq.heapify(sticks)
+        res = 0
+        
+        while len(sticks) > 1:
+            short1, short2 = heapq.heappop(sticks), heapq.heappop(sticks)
+            res += short1 + short2
+            heapq.heappush(sticks, short1+short2)
+        
+        return res
+        
