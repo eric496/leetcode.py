c3e53d915c92db27b6ef8aceadd8ab7ce535988b
eric496 <eric.mlengineer@gmail.com>
2020-03-03 08:55:17 -0500

    Modify solution

--- tree/108.convert_sorted_array_to_binary_search_tree.py
+++ tree/108.convert_sorted_array_to_binary_search_tree.py
@@ -15,36 +15,25 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
  -10  5
 '''
 
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-# Recursive solution
-class Solution:
-    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
-        return self.buildTree(nums)
 
-    def buildTree(self, nums: List[int]) -> TreeNode:
-        if not nums:
-            return None
-        
-        mid = len(nums) // 2
-        node = TreeNode(nums[mid])
-        node.left = self.buildTree(nums[:mid])
-        node.right = self.buildTree(nums[mid+1:])
-        
-        return node
-
-# More concise
+# DFS
 class Solution:
     def sortedArrayToBST(self, nums: list) -> TreeNode:
         if not nums:
             return None
+
         mid = len(nums) // 2
         root = TreeNode(nums[mid])
         root.left = self.sortedArrayToBST(nums[:mid])
+        # Start from mid+1 because mid is the root
         root.right = self.sortedArrayToBST(nums[mid+1:])
+
         return root
