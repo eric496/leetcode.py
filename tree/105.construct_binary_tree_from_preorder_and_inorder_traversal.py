368711aae7b21fe993ee22aeba7500d2ea9fa987
eric496 <eric.mlengineer@gmail.com>
2020-03-03 23:40:26 -0500

    Add solution

--- tree/105.construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ tree/105.construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -15,21 +15,47 @@ Return the following binary tree:
     /  \
    15   7
 """
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 
-# Recursive 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+# DFS 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         if not inorder:
             return None
-            
-        ix = inorder.index(preorder.pop(0))
-        root = TreeNode(inorder[ix])
-        root.left = self.buildTree(preorder, inorder[:ix])
-        root.right = self.buildTree(preorder, inorder[ix+1:])
+        
+        root = TreeNode(preorder.pop(0))
+        idx = inorder.index(root.val)
+        root.left = self.buildTree(preorder, inorder[:idx])
+        root.right = self.buildTree(preorder, inorder[idx+1:])
+        
         return root
+
+
+# Improved
+from collections import deque
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        preorder = deque(preorder)
+        
+        return self.build(preorder, inorder)
+    
+    
+    def build(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if not preorder or not inorder:
+            return None
+        
+        idx = inorder.index(preorder.popleft())
+        root = TreeNode(inorder[idx])
+        root.left = self.build(preorder, inorder[:idx])
+        root.right = self.build(preorder, inorder[idx+1:])
+        
+        return root
+        
