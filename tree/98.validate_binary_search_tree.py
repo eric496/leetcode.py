45651a6c4956d584c65ab609d72316c92eff776f
eric496 <eric.mlengineer@gmail.com>
2020-03-04 17:25:25 -0500

    Add solution

--- tree/98.validate_binary_search_tree.py
+++ tree/98.validate_binary_search_tree.py
@@ -6,15 +6,13 @@ The right subtree of a node contains only nodes with keys greater than the node'
 Both the left and right subtrees must also be binary search trees.
 
 Example 1:
-
 Input:
-    2
+    2s
    / \
   1   3
 Output: true
 
 Example 2:
-
     5
    / \
   1   4
@@ -25,13 +23,16 @@ Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
              is 5 but its right child's value is 4.
 """
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+# Solution 1: recursive
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         return self.validate(root, float('-inf'), float('inf'))
@@ -45,3 +46,24 @@ class Solution:
 
         return self.validate(root.left, lower, min(upper, root.val)) \
                 and self.validate(root.right, max(lower, root.val), upper)
+
+
+# Solution 2: iterative
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        stk = []
+        prev = None
+        
+        while root or stk:
+            while root:
+                stk.append(root)
+                root = root.left
+                
+            node = stk.pop()
+            if prev and node.val <= prev.val:
+                return False
+            prev = node
+            root = node.right
+            
+        return True
+        
