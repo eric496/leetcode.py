0da5adb388e7c0aceafbcb4b0128f46c4482d217
eric496 <eric.mlengineer@gmail.com>
2020-03-04 18:07:33 -0500

    Modify solution

--- tree/103.binary_tree_zigzag_level_order_traversal.py
+++ tree/103.binary_tree_zigzag_level_order_traversal.py
@@ -16,12 +16,14 @@ return its zigzag level order traversal as:
 ]
 """
 
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 from collections import deque
 
@@ -31,22 +33,25 @@ class Solution:
             return []
 
         q, res = deque([root]), []
-        odd_level = False
+        reverse = False
 
         while q:
-            level, size = [], len(q)
-            for i in range(size):
+            level = []
+            
+            for _ in range(len(q)):
                 node = q.popleft()
                 level.append(node.val)
                 if node.left:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
-            if odd_level:
+            
+            if reverse:
                 res.append(level[::-1])
             else:
                 res.append(level)
-            odd_level = not odd_level
+            
+            reverse = not reverse
 
         return res
 
