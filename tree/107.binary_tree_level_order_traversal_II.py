f0cff88b6e47bf167ce04039707de6f8a30d34d4
eric496 <eric.mlengineer@gmail.com>
2020-03-04 22:11:37 -0500

    Modify solution

--- tree/107.binary_tree_level_order_traversal_II.py
+++ tree/107.binary_tree_level_order_traversal_II.py
@@ -19,35 +19,14 @@ return its bottom-up level order traversal as:
 '''
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-class Solution:
-    def levelOrderBottom(self, root: TreeNode) -> list:
-        if not root:
-            return []
 
-        level, result = [root], [[root.val]]
-        while level:
-            cur_level = []
-            is_there_item_in_next_level = 0
-            for item in level:
-                if (item.left or item.right) and not is_there_item_in_next_level:
-                    result.append([])
-                    is_there_item_in_next_level = 1
-                if item.left:
-                    cur_level.append(item.left)
-                    result[-1].append(item.left.val)
-                if item.right:
-                    cur_level.append(item.right)
-                    result[-1].append(item.right.val)
-            level = cur_level
-        return result[::-1]
-
-# Solution 2
+# Solution 1
 from collections import deque
 
 class Solution:
@@ -58,25 +37,63 @@ class Solution:
         q, res = deque([root]), []
         
         while q:
-            level, size = [], len(q)
-            for i in range(size):
+            level = []
+
+            for _ in range(len(q)):
                 node = q.popleft()
                 level.append(node.val)
+                
                 if node.left:
                     q.append(node.left)
+                
                 if node.right:
                     q.append(node.right)
+            
             res.append(level)
         
         return res[::-1]
+
+
+# Solution 2
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> list:
+        if not root:
+            return []
+
+        level, result = [root], [[root.val]]
+        
+        while level:
+            cur_level = []
+            is_there_item_in_next_level = 0
+            
+            for item in level:
+                if (item.left or item.right) and not is_there_item_in_next_level:
+                    result.append([])
+                    is_there_item_in_next_level = 1
+                
+                if item.left:
+                    cur_level.append(item.left)
+                    result[-1].append(item.left.val)
+                
+                if item.right:
+                    cur_level.append(item.right)
+                    result[-1].append(item.right.val)
+            
+            level = cur_level
+        
+        return result[::-1]
+
 
 # Solution 3: more concise but less readable
 class Solution:
     def levelOrderBottom(self, root: TreeNode) -> list:
         if not root:
             return []
-        q, result = [root], []
+
+        q, res = [root], []
+        
         while q:
-            result.append([node.val for node in level])
+            res.append([node.val for node in level])
             q = [node for el in q for node in (el.left, el.right) if node]
-        return result[::-1]
+
+        return res[::-1]
