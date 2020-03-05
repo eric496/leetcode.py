42e65c5983912969a2ffe21a9e41d65907de857c
eric496 <eric.mlengineer@gmail.com>
2020-03-04 23:02:47 -0500

    Modify solution

--- tree/110.balanced_binary_tree.py
+++ tree/110.balanced_binary_tree.py
@@ -4,7 +4,6 @@ For this problem, a height-balanced binary tree is defined as:
 a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
 
 Example 1:
-
 Given the following tree [3,9,20,null,null,15,7]:
 
     3
@@ -15,7 +14,6 @@ Given the following tree [3,9,20,null,null,15,7]:
 Return true.
 
 Example 2:
-
 Given the following tree [1,2,2,3,3,null,null,4,4]:
 
        1
@@ -28,12 +26,14 @@ Given the following tree [1,2,2,3,3,null,null,4,4]:
 Return false.
 '''
 
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 # Recursive solution
 class Solution:
@@ -53,6 +53,7 @@ class Solution:
     def getHeight(self, root: TreeNode) -> int:
         if not root:
             return 0
+
         left = self.getHeight(root.left)
         right = self.getHeight(root.right)
         
