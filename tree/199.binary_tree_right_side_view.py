c68f70df8ad175a6fd24b969a90362850e2d1fb0
eric496 <eric.mlengineer@gmail.com>
2020-03-08 17:14:29 -0400

    Modify solution

--- tree/199.binary_tree_right_side_view.py
+++ tree/199.binary_tree_right_side_view.py
@@ -14,29 +14,58 @@ Explanation:
 """
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-# Iterative
+
+# Solution 1: iterative - a variation of level order traversal
+from collections import deque
+
 class Solution:
     def rightSideView(self, root: TreeNode) -> List[int]:
         if not root:
             return []
         
-        q, res = [root], []
+        res = []
+        q = deque([root])
         
         while q:
-            level, size = [], len(q)
+            size = len(q)
+            
             for i in range(size):
-                node = q.pop(0)
-                level.append(node.val)
+                node = q.popleft()
+                
+                # Only need to store the last node val of each level
+                if i == size-1: 
+                    res.append(node.val)    
+                
                 if node.left:
                     q.append(node.left)
+                
                 if node.right:
                     q.append(node.right)
-            res.append(level)
             
-        return [ls[-1] for ls in res]
+        return res
+
+
+# Solution 2: recursive
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        res = []
+        self.dfs(root, 0, res)
+        
+        return res
+        
+        
+    def dfs(self, node: TreeNode, depth: int, res: List[int]) -> None:
+        if not node:
+            return
+        
+        if depth == len(res):
+            res.append(node.val)
+            
+        self.dfs(node.right, depth+1, res)
+        self.dfs(node.left, depth+1, res)
