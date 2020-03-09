e0ddf2aa0c8de38cb41a5e16e6f332f5348e9494
eric496 <eric.mlengineer@gmail.com>
2020-03-09 10:59:08 -0400

    Add recursive sln

--- tree/235.lowest_common_ancestor_of_a_binary_search_tree.py
+++ tree/235.lowest_common_ancestor_of_a_binary_search_tree.py
@@ -12,13 +12,11 @@ Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
          3   5
 
 Example 1:
-
 Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
 Output: 6
 Explanation: The LCA of nodes 2 and 8 is 6.
 
 Example 2:
-
 Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
 Output: 2
 Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself 
@@ -29,13 +27,28 @@ All of the nodes' values will be unique.
 p and q are different and both values will exist in the BST.
 '''
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+# Solution 1: recursive
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if p.val < root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        
+        if p.val > root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        
+        return root
+
+
+# Solution 2: iterative
 class Solution:
     def lowestCommonAncestor(self, root:TreeNode, p:TreeNode, q:TreeNode) -> TreeNode:
         while root:
