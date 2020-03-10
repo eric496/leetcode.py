3e2bd3526ebf51ee4ab25eb72e65a5f2c219e72d
eric496 <eric.mlengineer@gmail.com>
2020-03-10 16:24:03 -0400

    Add recursive sln

--- tree/404.sum_of_left_leaves.py
+++ tree/404.sum_of_left_leaves.py
@@ -13,41 +13,50 @@ There are two left leaves in the binary tree, with values 9 and 15 respectively.
 '''
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-# first thought
+
+# Solution 1: recursive
 class Solution:
     def sumOfLeftLeaves(self, root: TreeNode) -> int:
-        queue = [root]
-        result = 0
-        while queue:
-            length = len(queue)
-            for _ in range(length):
-                node = queue.pop(0)
-                result += node.left.val if node and node.left and (not node.left.left) and (not node.left.right) else 0
-                if node and node.left:
-                    queue.append(node.left)
-                if node and node.right:
-                    queue.append(node.right)
-        return result
+        return self.dfs(root, False)
+    
+    
+    def dfs(self, node: TreeNode, is_left: bool) -> int:
+        if not node:
+            return 0
+        
+        if not node.left and not node.right and is_left:
+            return node.val
+        
+        return self.dfs(node.left, True) + self.dfs(node.right, False)
 
-# optimize a little bit
+
+# Solution 2: iterative
 class Solution:
     def sumOfLeftLeaves(self, root: TreeNode) -> int:
-        queue = [root]
-        result = 0
-        while queue:
-            length = len(queue)
-            for _ in range(length):
-                node = queue.pop(0)
-                if node:
-                    if node.left:
-                        result += node.left.val if (not node.left.left) and (not node.left.right) else 0
-                        queue.append(node.left) 
-                    if node.right:
-                        queue.append(node.right)
-        return result
+        if not root:
+            return 0
+        
+        stk = [root]
+        res = 0
+        
+        while stk:
+            node = stk.pop()
+            
+            if node.left and not node.left.left and not node.left.right:
+                res += node.left.val
+            
+            if node.left:
+                stk.append(node.left)
+            
+            if node.right:
+                stk.append(node.right)
+                
+        return res
+
+
