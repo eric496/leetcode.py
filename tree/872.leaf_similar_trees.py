0a0294efe059a4142bb01002364d7c89de558d94
eric496 <eric.mlengineer@gmail.com>
2020-03-17 23:48:12 -0400

    Add sln

--- tree/872.leaf_similar_trees.py
+++ tree/872.leaf_similar_trees.py
@@ -10,17 +10,41 @@ Both of the given trees will have between 1 and 100 nodes.
 """
 
 
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
+
+# Solution 1: inorder traversal 
+class Solution:
+    def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
+        leaves1, leaves2 = [], []
+        self.getLeaves(root1, leaves1)
+        self.getLeaves(root2, leaves2)
+        
+        return leaves1 == leaves2
+        
+        
+    def getLeaves(self, root: TreeNode, leaves: List[int]) -> None:
+        if not root:
+            return 
+        
+        if not root.left and not root.right:
+            leaves.append(root.val)
+            
+        self.getLeaves(root.left, leaves)
+        self.getLeaves(root.right, leaves)
+
+
+# Solution 2: more concise
 class Solution:
     def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
         return self.dfs(root1) == self.dfs(root2)
 
-    def dfs(self, node: TreeNode) -> TreeNode:
+
+    def dfs(self, node: TreeNode) -> List[int]:
         if not node:
             return []
 
@@ -28,4 +52,3 @@ class Solution:
             return [node.val]
 
         return self.dfs(node.left) + self.dfs(node.right)
-
