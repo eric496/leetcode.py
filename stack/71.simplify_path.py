69d6d29d272750d518a7a78a00beee16fb2b89d4
eric496 <eric.mlengineer@gmail.com>
2020-03-23 15:02:30 -0400

    Modify sln

--- stack/71.simplify_path.py
+++ stack/71.simplify_path.py
@@ -1,11 +1,7 @@
 """
 Given an absolute path for a file (Unix-style), simplify it. Or in other words, convert it to the canonical path.
-
 In a UNIX-style file system, a period . refers to the current directory. Furthermore, a double period .. moves the directory up a level. For more information, see: Absolute path vs relative path in Linux/Unix
-
 Note that the returned canonical path must always begin with a slash /, and there must be only a single slash / between two directory names. The last directory name (if it exists) must not end with a trailing /. Also, the canonical path must be the shortest string representing the absolute path.
-
-
 
 Example 1:
 Input: "/home/"
@@ -35,19 +31,20 @@ Input: "/a//b////c/d//././/.."
 Output: "/a/b/c"
 """
 
+
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        sp = path.split("/")
+        split = path.split('/')
         stk = []
-
-        for p in sp:
-            if p == "/" or p == "." or not p:
+        
+        for s in split:
+            if s in {'/', '.', ''}:
                 continue
-            elif p == "..":
+            elif s == '..':
                 if stk:
                     stk.pop()
             else:
-                stk.append(p)
-
-        return "/"+"/".join(stk)
-
+                stk.append(s)
+                
+        return '/' + '/'.join(stk)
+        
