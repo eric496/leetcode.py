70a25ba69a6abe0ee039c665929f287af94a0fb7
eric496 <eric.mlengineer@gmail.com>
2020-04-11 19:33:47 -0400

    Modify solution

--- binary_search/33.search_in_rotated_sorted_array.py
+++ binary_search/33.search_in_rotated_sorted_array.py
@@ -19,15 +19,16 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         if not nums:
             return -1
-
-        low, high = 0, len(nums) - 1
-
+        
+        low = 0
+        high = len(nums) - 1
+        
         while low <= high:
-            mid = low + ((high - low) >> 1)
-
-            if target == nums[mid]:
+            mid = low + ((high-low) >> 1)
+            
+            if nums[mid] == target:
                 return mid
-            elif nums[low] <= nums[mid]:
+            elif nums[mid] >= nums[low]:
                 if nums[low] <= target <= nums[mid]:
                     high = mid - 1
                 else:
@@ -37,5 +38,5 @@ class Solution:
                     low = mid + 1
                 else:
                     high = mid - 1
-
+        
         return -1
