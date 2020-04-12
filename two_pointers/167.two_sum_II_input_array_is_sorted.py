baaa931b77acb360217cbc29d25861b36bea1676
eric496 <eric.mlengineer@gmail.com>
2020-04-11 20:00:01 -0400

    Format description and solution

--- two_pointers/167.two_sum_II_input_array_is_sorted.py
+++ two_pointers/167.two_sum_II_input_array_is_sorted.py
@@ -7,11 +7,11 @@ Your returned answers (both index1 and index2) are not zero-based.
 You may assume that each input would have exactly one solution and you may not use the same element twice.
 
 Example:
-
 Input: numbers = [2,7,11,15], target = 9
 Output: [1,2]
 Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 """
+
 
 # Solution 1: two pointers - O(n) TC
 class Solution:
