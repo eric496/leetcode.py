c5022e54885e0f85c15c046d23927621893367f5
eric496 <eric.mlengineer@gmail.com>
2020-04-11 22:38:27 -0400

    Add solutions

--- array/349.intersection_of_two_arrays.py
+++ array/349.intersection_of_two_arrays.py
@@ -9,32 +9,69 @@ Each element in the result must be unique.
 The result can be in any order.
 """
 
-# O(mn) time complexity
+# Solution 1: brute force - O(mn) TC
 class Solution:
-    def intersection(self, nums1: list, nums2: list) -> list:
-        res = []
-        for n in nums1:
-            if n in nums2:
-                res.append(n)
-                nums2.remove(n)
-        return list(set(res))
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = set()
+        
+        for n1 in nums1:
+            for n2 in nums2:
+                if n1 == n2:
+                    res.add(n1)
+                    
+        return list(res)
 
 
-# sort first, max(O(nlogn), O(mlogm)) time complexity
+# Solution 2: two pointers, max(O(nlogn), O(mlogm)) TC
 class Solution:
-    def intersection(self, nums1: list, nums2: list) -> list:
-        res = []
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         nums1.sort()
         nums2.sort()
-        p1 = p2 = 0
-        while p1 < len(nums1) and p2 < len(nums2):
-            if nums1[p1] == nums2[p2]:
-                if nums1[p1] not in res:
-                    res.append(nums1[p1])
-                    p1 += 1
-                    p2 += 1
-            elif nums1[p1] < nums2[p2]:
-                p1 += 1
-            else:
-                p2 += 1
-        return res
+        i = j = 0
+        res = set()
+        
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                res.add(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            
+        return list(res)
+
+
+# Solution 3: binary search - O(nlogm) TC
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = set()
+        nums2.sort()
+        
+        for n in nums1:
+            if self.binarySearch(nums2, n):
+                res.add(n)
+                
+        return list(res)
+        
+    def binarySearch(self, nums: List[int], target: int) -> bool:
+        low, high = 0, len(nums) - 1
+        
+        while low <= high:
+            mid = low + ((high-low) >> 1)
+            
+            if nums[mid] == target:
+                return True
+            elif nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
+        
+        return False
+
+
+# Solution 4: two sets - O(m+n) TC
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
