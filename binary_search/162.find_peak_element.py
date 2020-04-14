03c5453342387b610b9bd34bf2a1b9003da7a5ba
eric496 <eric.mlengineer@gmail.com>
2020-04-13 23:35:35 -0400

    Modify solution

--- binary_search/162.find_peak_element.py
+++ binary_search/162.find_peak_element.py
@@ -23,15 +23,14 @@ Your solution should be in logarithmic complexity.
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         low, high = 0, len(nums) - 1
-
+        
         while low < high:
-            mid = low + (high - low) // 2
-
-            if nums[mid] > nums[mid + 1] and nums[mid] > nums[mid - 1]:
-                return mid
-            elif nums[mid] < nums[mid + 1]:
+            mid = low + ((high-low) >> 1)
+            
+            if nums[mid] < nums[mid+1]:
                 low = mid + 1
             else:
-                high = mid - 1
-
-        return low if nums[low] >= nums[high] else high
+                high = mid
+                
+        return low
+        
