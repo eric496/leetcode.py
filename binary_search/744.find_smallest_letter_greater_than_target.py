81d942858f97f89a0918f2e165a4a64cf4bbde86
eric496 <eric.mlengineer@gmail.com>
2020-04-18 17:34:40 -0400

    Add solutions

--- binary_search/744.find_smallest_letter_greater_than_target.py
+++ binary_search/744.find_smallest_letter_greater_than_target.py
@@ -40,6 +40,7 @@ target is a lowercase letter.
 """
 
 
+# Solution 1
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
         if ord(target) >= ord(letters[-1]):
@@ -55,3 +56,20 @@ class Solution:
                 high = mid
 
         return letters[high]
+
+
+# Solution 2: optimized
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        low, high = 0, len(letters)
+        
+        while low < high:
+            mid = low + ((high-low) >> 1)
+            
+            if ord(letters[mid]) > ord(target):
+                high = mid
+            else:
+                low = mid + 1
+                
+        return letters[low%len(letters)]
+        
