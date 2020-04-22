3e497eaec233571d76ac933c166e5ab0740d4a38
eric496 <eric.mlengineer@gmail.com>
2020-04-22 13:27:51 -0400

    Modify solution

--- binary_search/4.median_of_two_sorted_arrays.py
+++ binary_search/4.median_of_two_sorted_arrays.py
@@ -14,46 +14,69 @@ nums2 = [3, 4]
 The median is (2 + 3)/2 = 2.5
 """
 
-# Brute force
+# Solution 1: merge sort - O(m + n) TC and O(m + n) SC
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        comb = sorted(nums1 + nums2)
-        if len(comb) % 2:
-            return comb[len(comb) // 2]
+        m, n = len(nums1), len(nums2)
+        res = self.merge(nums1, nums2)
+        
+        if (m + n) & 1:
+            return res[m + n >> 1]
         else:
-            return (comb[len(comb) // 2] + comb[len(comb) // 2 - 1]) / 2
-
-
-# Binary search O(log(min(m,n)))
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) < len(nums2):
-            short = nums1
-            long = nums2
-        else:
-            short = nums2
-            long = nums1
-
-        len1, len2 = len(short), len(long)
-        total_len = len1 + len2
-
-        start, end = 0, len(short)
-
-        while start <= end:
-            mid1 = start + (end - start) // 2
-            mid2 = (total_len + 1) // 2 - mid1
-
-            maxleft1 = float("-inf") if not mid1 else short[mid1 - 1]
-            minright1 = float("inf") if mid1 == len1 else short[mid1]
-            maxleft2 = float("-inf") if not mid2 else long[mid2 - 1]
-            minright2 = float("inf") if mid2 == len2 else long[mid2]
-
-            if maxleft1 <= minright2 and maxleft2 <= minright1:
-                if total_len % 2:
-                    return float(max(maxleft1, maxleft2))
-                else:
-                    return (max(maxleft1, maxleft2) + min(minright1, minright2)) / 2
-            elif maxleft1 > minright2:
-                end = mid1 - 1
+            return (res[(m + n >> 1) - 1] + res[m + n >> 1]) / 2
+        
+    def merge(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = [0] * (len(nums1) + len(nums2))
+        i = j = k = 0
+        
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                res[k] = nums1[i]
+                i += 1
             else:
-                start = mid1 + 1
+                res[k] = nums2[j]
+                j += 1
+            
+            k += 1
+        
+        while i < len(nums1):
+            res[k] = nums1[i]
+            i += 1
+            k += 1
+        
+        while j < len(nums2):
+            res[k] = nums2[j]
+            j += 1
+            k += 1
+        
+        return res
+
+
+# Solution 2: binary search O(log(m + n))
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+
+        m, n = len(nums1), len(nums2)
+
+        low, high = 0, m
+
+        while low <= high:
+            partition1 = low + (high - low >> 1)
+            partition2 = (m + n + 1 >> 1) - partition1
+
+            max_left1 = float("-inf") if partition1 == 0 else nums1[partition1 - 1]
+            min_right1 = float("inf") if partition1 == m else nums1[partition1]
+            max_left2 = float("-inf") if partition2 == 0 else nums2[partition2 - 1]
+            min_right2 = float("inf") if partition2 == n else nums2[partition2]
+
+            if max_left1 <= min_right2 and max_left2 <= min_right1:
+                if (m + n) & 1:
+                    return float(max(max_left1, max_left2))
+                else:
+                    return (max(max_left1, max_left2) + min(min_right1, min_right2)) / 2
+            elif max_left1 > min_right2:
+                high = partition1 - 1
+            else:
+                low = partition1 + 1
