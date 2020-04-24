411d7e224aacfc512b8c1d24406e791bfb02bb76
eric496 <eric.mlengineer@gmail.com>
2020-04-24 14:07:50 -0400

    Modify solution

--- binary_search/74.search_a_2d_matrix.py
+++ binary_search/74.search_a_2d_matrix.py
@@ -26,47 +26,43 @@ Output: false
 """
 
 
+# Solution 1: do a lower bound search on the first column and a normal binary search on the target row
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         if not matrix or not matrix[0]:
             return False
-
-        first_col = [row[0] for row in matrix]
-        low, high = 0, len(first_col) - 1
-
+        
+        row = self.lower_bound([row[0] for row in matrix], target)
+        
+        return False if row == -1 else self.binary_search(matrix[row], target) 
+    
+    def lower_bound(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums)
+        
         while low < high:
-            mid = low + (high - low) // 2
-            if first_col[mid] == target:
-                return True
-            elif first_col[mid] > target:
-                high = mid - 1
+            mid = low + (high - low >> 1)
+                            
+            if nums[mid] > target:
+                high = mid 
             else:
                 low = mid + 1
-
-        # Need to compare with the high positioned element
-        if target == first_col[high]:
-            return True
-        elif target > first_col[high]:
-            idx = high
-        else:
-            idx = high - 1 if high > 0 else 0
-            if matrix[idx][-1] < target:
-                return False
-
-        search_row = matrix[idx]
-        low, high = 0, len(search_row) - 1
-
-        while low < high:
-            mid = low + (high - low) // 2
-            if search_row[mid] == target:
+        
+        return low - 1
+        
+    def binary_search(self, nums: List[int], target: int) -> bool:
+        low, high = 0, len(nums) - 1
+        
+        while low <= high:
+            mid = low + (high - low >> 1)
+            
+            if nums[mid] == target:
                 return True
-            elif search_row[mid] > target:
+            elif nums[mid] > target:
                 high = mid - 1
-            else:
+            elif nums[mid] < target:
                 low = mid + 1
-
-        # Need to compare with the high positioned element
-        return True if search_row[high] == target else False
+        
+        return False
 
 
 # A more concise solution
@@ -74,19 +70,20 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         if not matrix or not matrix[0]:
             return False
-
+        
         m, n = len(matrix), len(matrix[0])
         low, high = 0, m * n - 1
-
+        
         while low <= high:
-            mid = low + ((high - low) >> 1)
-            num = matrix[mid // n][mid % n]
-
-            if num == target:
+            mid = low + (high - low >> 1)
+            mid_val = matrix[mid // n][mid % n]
+            
+            if mid_val == target:
                 return True
-            elif num < target:
-                low = mid + 1
-            else:
+            elif mid_val > target:
                 high = mid - 1
-
+            elif mid_val < target:
+                low = mid + 1
+                
         return False
+        
