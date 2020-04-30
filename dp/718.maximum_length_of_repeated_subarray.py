3e583b71c5b76357bbe63ea4269d403079fc2e03
eric496 <eric.mlengineer@gmail.com>
2020-04-30 17:30:38 -0400

    Modify solution

--- dp/718.maximum_length_of_repeated_subarray.py
+++ dp/718.maximum_length_of_repeated_subarray.py
@@ -17,11 +17,14 @@ Note:
 
 class Solution:
     def findLength(self, A: List[int], B: List[int]) -> int:
-        dp = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
-
-        for i in range(1, len(A) + 1):
-            for j in range(1, len(B) + 1):
+        m, n = len(A), len(B)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        res = 0
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
                 if A[i - 1] == B[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1] + 1
-
-        return max(max(row) for row in dp)
+                    res = max(res, dp[i][j])
+                    
+        return res
