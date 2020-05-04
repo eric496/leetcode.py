b1485caaa1bc3d658dd1693916d7bc2689b25ba1
eric496 <eric.mlengineer@gmail.com>
2020-05-04 19:57:29 -0400

    Add 1337

---
+++ binary_search/1337.the_K_weakest_rows_in_a_matrix.py
@@ -0,0 +1,70 @@
+"""
+Given a m * n matrix mat of ones (representing soldiers) and zeros (representing civilians), return the indexes of the k weakest rows in the matrix ordered from the weakest to the strongest.
+A row i is weaker than row j, if the number of soldiers in row i is less than the number of soldiers in row j, or they have the same number of soldiers but i is less than j. Soldiers are always stand in the frontier of a row, that is, always ones may appear first and then zeros.
+
+Example 1:
+Input: mat = 
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]], 
+k = 3
+Output: [2,0,3]
+Explanation: 
+The number of soldiers for each row is: 
+row 0 -> 2 
+row 1 -> 4 
+row 2 -> 1 
+row 3 -> 2 
+row 4 -> 5 
+Rows ordered from the weakest to the strongest are [2,0,3,1,4]
+
+Example 2:
+Input: mat = 
+[[1,0,0,0],
+ [1,1,1,1],
+ [1,0,0,0],
+ [1,0,0,0]], 
+k = 2
+Output: [0,2]
+Explanation: 
+The number of soldiers for each row is: 
+row 0 -> 1 
+row 1 -> 4 
+row 2 -> 1 
+row 3 -> 1 
+Rows ordered from the weakest to the strongest are [0,2,3,1]
+
+Constraints:
+m == mat.length
+n == mat[i].length
+2 <= n, m <= 100
+1 <= k <= m
+matrix[i][j] is either 0 or 1.
+"""
+
+
+import heapq as hq
+
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        pq = [(self.count_ones(row), i) for i, row in enumerate(mat)]
+        hq.heapify(pq)
+        
+        return [hq.heappop(pq)[1] for _ in range(k)]
+            
+    def count_ones(self, nums: List[int]) -> int:
+        lo, hi = 0, len(nums)
+        
+        while lo < hi:
+            mid = lo + (hi - lo >> 1)
+            
+            if nums[mid] == 1:
+                lo = mid + 1
+            else:
+                hi = mid
+            
+        return lo
+        
