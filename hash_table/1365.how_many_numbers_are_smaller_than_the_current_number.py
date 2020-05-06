0bbbec000ebda943d46b889f8784e3e512f45aab
eric496 <eric.mlengineer@gmail.com>
2020-05-06 12:18:09 -0400

    Add 1365

---
+++ hash_table/1365.how_many_numbers_are_smaller_than_the_current_number.py
@@ -0,0 +1,58 @@
+"""
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+Return the answer in an array.
+
+Example 1:
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation: 
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1). 
+For nums[3]=2 there exist one smaller number than it (1). 
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+Example 2:
+Input: nums = [6,5,4,8]
+Output: [2,1,0,3]
+
+Example 3:
+Input: nums = [7,7,7,7]
+Output: [0,0,0,0]
+
+Constraints:
+2 <= nums.length <= 500
+0 <= nums[i] <= 100
+"""
+
+
+# Solution 1: sort
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        sorted_nums = sorted(nums)
+        cnt = {}
+        
+        for i, num in enumerate(sorted_nums):
+            if num not in cnt:
+                cnt[num] = i
+                
+        return [cnt[n] for n in nums]
+        
+
+# Solution 2: O(n) TC
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        cnt = [0] * 101
+        
+        for num in nums:
+            cnt[num] += 1
+            
+        cursum = 0
+        presum = [0] * 101
+        
+        for i in range(len(cnt)):
+            presum[i] = cursum
+            cursum += cnt[i]
+        
+        return [presum[n] for n in nums]
+            
