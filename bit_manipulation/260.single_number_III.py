4afafa5cfc4228f1556a0f60bce810cb44d96e8b
eric496 <eric.mlengineer@gmail.com>
2020-05-07 14:39:27 -0400

    Add 260

---
+++ bit_manipulation/260.single_number_III.py
@@ -0,0 +1,49 @@
+"""
+Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+
+Example:
+Input:  [1,2,1,3,2,5]
+Output: [3,5]
+
+Note:
+The order of the result is not important. So in the above example, [5, 3] is also correct.
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+"""
+
+
+# Solution 1: use hashmap - O(n) TC and O(n) SC
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        cnt = {}
+        
+        for num in nums:
+            cnt[num] = cnt.get(num, 0) + 1
+        
+        res = []
+        
+        for k, v in cnt.items():
+            if v == 1:
+                res.append(k)
+                
+        return res
+
+
+# Solution 2: bit manipulation
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+        
+        for num in nums:
+            xor ^= num
+            
+        last_set_bit = xor & ~xor + 1
+        res = [0, 0]
+        
+        for num in nums:
+            if num & last_set_bit:
+                res[0] ^= num
+            else:
+                res[1] ^= num
+                
+        return res
+        
