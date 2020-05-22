4581105a904802ff0d21ae2ee0e3ccc48f2d3735
eric496 <eric.mlengineer@gmail.com>
2020-05-22 11:07:43 -0400

    Modify solution

--- hash_table/451.sort_characters_by_freqency.py
+++ hash_table/451.sort_characters_by_freqency.py
@@ -33,10 +33,11 @@ Note that 'A' and 'a' are treated as two different characters.
 class Solution:
     def frequencySort(self, s: str) -> str:
         freq = {}
-
-        for ch in s:
-            freq[ch] = freq.get(ch, 0) + 1
-
-        sort_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-
-        return "".join(x[0] * x[1] for x in sort_freq)
+        
+        for c in s:
+            freq[c] = freq.get(c, 0) + 1
+            
+        sort_by_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+        
+        return "".join(c * n for c, n in sort_by_freq)
+        
