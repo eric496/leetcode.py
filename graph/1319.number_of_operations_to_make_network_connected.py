ff9be099cf5b931ccddf17d1098358deefb76a73
eric496 <eric.mlengineer@gmail.com>
2020-05-30 20:40:08 -0400

    Add 1319

---
+++ graph/1319.number_of_operations_to_make_network_connected.py
@@ -0,0 +1,75 @@
+"""
+There are n computers numbered from 0 to n-1 connected by ethernet cables connections forming a network where connections[i] = [a, b] represents a connection between computers a and b. Any computer can reach any other computer directly or indirectly through the network.
+Given an initial computer network connections. You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected. Return the minimum number of times you need to do this in order to make all the computers connected. If it's not possible, return -1. 
+
+Example 1:
+Input: n = 4, connections = [[0,1],[0,2],[1,2]]
+Output: 1
+Explanation: Remove cable between computer 1 and 2 and place between computers 1 and 3.
+
+Example 2:
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+Output: 2
+
+Example 3:
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
+Output: -1
+Explanation: There are not enough cables.
+
+Example 4:
+Input: n = 5, connections = [[0,1],[0,2],[3,4],[2,3]]
+Output: 0
+
+Constraints:
+1 <= n <= 10^5
+1 <= connections.length <= min(n*(n-1)/2, 10^5)
+connections[i].length == 2
+0 <= connections[i][0], connections[i][1] < n
+connections[i][0] != connections[i][1]
+There are no repeated connections.
+No two computers are connected by more than one cable.
+"""
+
+
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        parent = list(range(n))
+        rank = [0] * n
+        cables =  0
+        
+        for u, v in connections:
+            if not self.union(u, v, parent, rank):
+                cables += 1
+                
+        components = set()  
+    
+        for p in parent:
+            components.add(self.find(p, parent))
+            
+        return len(components) - 1 if cables - len(components) + 1 >= 0 else -1 
+        
+    def find(self, u: int, parent: List[int]) -> int:
+        if u == parent[u]:
+            return u
+        else:
+            root = self.find(parent[u], parent)
+            parent[u] = root
+            return root
+    
+    def union(self, u: int, v: int, parent: List[int], rank: List[int]) -> bool:
+        u_root = self.find(u, parent)
+        v_root = self.find(v, parent)
+        
+        if u_root == v_root:
+            return False
+        
+        if rank[u_root] > rank[v_root]:
+            parent[v_root] = u_root
+        elif rank[u_root] < rank[v_root]:
+            parent[u_root] = v_root
+        else:
+            parent[v_root] = u_root
+            rank[u_root] += 1
+            
+        return True
+        
