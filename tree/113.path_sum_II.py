af4382e8fd5b64c50d319f790b09a90387d7e70f
eric496 <eric.mlengineer@gmail.com>
2020-06-03 13:41:59 -0400

    Modify solution

--- tree/113.path_sum_II.py
+++ tree/113.path_sum_II.py
@@ -30,7 +30,7 @@ class TreeNode:
         self.right = None
 
 
-# Solution 1: recursive
+# Solution 1: recursive using subtraction
 class Solution:
     def pathSum(self, root: TreeNode, target: int) -> List[List[int]]:
         if not root:
@@ -44,15 +44,37 @@ class Solution:
     def dfs(
         self, node: TreeNode, target: int, path: List[int], res: List[List[int]]
     ) -> None:
+        if not node:
+            return
+        
         if node.val == target and not node.left and not node.right:
             path.append(node.val)
             res.append(path)
+            return
 
-        if node.left:
-            self.dfs(node.left, target - node.val, path + [node.val], res)
+        self.dfs(node.left, target - node.val, path + [node.val], res)
+        self.dfs(node.right, target - node.val, path + [node.val], res)
 
-        if node.right:
-            self.dfs(node.right, target - node.val, path + [node.val], res)
+
+# Solution 2: recursive using addition
+class Solution:
+    def pathSum(self, root: TreeNode, target: int) -> List[List[int]]:
+        res = []
+        self.dfs(root, 0, [], target, res)
+        
+        return res
+    
+    def dfs(self, root: TreeNode, cursum: int, path: List[int], target: int, res: List[int]) -> None:
+        if not root:
+            return
+        
+        if cursum + root.val == target and not root.left and not root.right:
+            path.append(root.val)
+            res.append(path)
+            return
+        
+        self.dfs(root.left, cursum + root.val, path + [root.val], target, res)
+        self.dfs(root.right, cursum + root.val, path + [root.val], target, res)
 
 
 # Solution 2: iterative
