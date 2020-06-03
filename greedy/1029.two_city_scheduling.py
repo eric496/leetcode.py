60615950e7f9559979dcd2721fee2f11fda65698
eric496 <eric.mlengineer@gmail.com>
2020-06-03 08:37:54 -0400

    Modify solution

--- greedy/1029.two_city_scheduling.py
+++ greedy/1029.two_city_scheduling.py
@@ -29,7 +29,8 @@ Thought process:
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         costs.sort(key=lambda x: x[1] - x[0], reverse=True)
-        cost_A = sum(cost[0] for cost in costs[: len(costs) // 2])
-        cost_B = sum(cost[1] for cost in costs[len(costs) // 2 :])
-
+        n = len(costs)
+        cost_A = sum(costs[i][0] for i in range(n//2))
+        cost_B = sum(costs[i][1] for i in range(n//2, n))
+        
         return cost_A + cost_B
