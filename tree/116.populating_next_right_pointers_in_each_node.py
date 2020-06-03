71e089f5c855d28ea80e75e842dd38d609c9b4b5
eric496 <eric.mlengineer@gmail.com>
2020-06-02 22:58:34 -0400

    Modify solution

--- tree/116.populating_next_right_pointers_in_each_node.py
+++ tree/116.populating_next_right_pointers_in_each_node.py
@@ -33,22 +33,21 @@ class Node:
 
 # Solution 1: recursive
 class Solution:
-    def connect(self, root: "Node") -> "Node":
+    def connect(self, root: 'Node') -> 'Node':
         if not root:
-            return None
-
+            return root
+        
         if not root.left and not root.right:
             return root
-
-        if root and root.left and root.right:
-            root.left.next = root.right
-
-            if root.next:
-                root.right.next = root.next.left
-
-            self.connect(root.left)
-            self.connect(root.right)
-
+        
+        root.left.next = root.right
+        
+        if root.next:
+            root.right.next = root.next.left
+            
+        self.connect(root.left)
+        self.connect(root.right)
+            
         return root
 
 
