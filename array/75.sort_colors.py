203165058989e923317ad0a09e7132ae3d832ef2
eric496 <eric.mlengineer@gmail.com>
2020-06-11 12:26:33 -0400

    Add solution

--- array/75.sort_colors.py
+++ array/75.sort_colors.py
@@ -14,21 +14,47 @@ First, iterate the array counting number of 0's, 1's, and 2's, then overwrite ar
 Could you come up with a one-pass algorithm using only constant space?
 """
 
-# Dutch National Flag problem (https://en.wikipedia.org/wiki/Dutch_national_flag_problem)
+
+# Solution 1: counting sort
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        red, white, blue = 0, 0, len(nums) - 1
+        cnt = [0] * 3
+        
+        for n in nums:
+            cnt[n] += 1
+        
+        c0, c1, c2 = cnt
+        
+        for i in range(c0):
+            nums[i] = 0
+            
+        for i in range(c0, c0 + c1):
+            nums[i] = 1
+            
+        for i in range(c0 + c1, c0 + c1 + c2):
+            nums[i] = 2
 
-        while white <= blue:
-            if nums[white] == 0:
-                nums[red], nums[white] = nums[white], nums[red]
-                red += 1
-                white += 1
-            elif nums[white] == 1:
-                white += 1
-            else:
-                nums[white], nums[blue] = nums[blue], nums[white]
-                blue -= 1
+
+# Dutch National Flag problem (https://en.wikipedia.org/wiki/Dutch_national_flag_problem)
+# Solution 2
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        lo, hi = 0, len(nums) - 1
+        i = lo
+        
+        while i <= hi:
+            if nums[i] == 0:
+                nums[lo], nums[i] = nums[i], nums[lo]
+                lo += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[hi] = nums[hi], nums[i]
+                hi -= 1
+            elif nums[i] == 1:
+                i += 1
