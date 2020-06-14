2513a624b3dbf16d6192bf1ce8f15656211f5ac7
eric496 <eric.mlengineer@gmail.com>
2020-06-13 20:57:57 -0400

    Add solution

--- stack/227.basic_calculator_II.py
+++ stack/227.basic_calculator_II.py
@@ -20,6 +20,55 @@ Do not use the eval built-in library function.
 """
 
 
+# Solution 1
+class Solution:
+    def calculate(self, s: str) -> int:
+        tokens = self.tokenize(s)
+        num_stk = []
+        op_stk = []
+        
+        for token in tokens:
+            if token.isdigit():
+                if op_stk and op_stk[-1] in "*/":
+                    op = op_stk.pop()
+                    left = abs(num_stk.pop())
+                    num_stk.append(left // int(token) if op == "/" else left * int(token))
+                else:
+                    num_stk.append(int(token))
+            else:
+                op_stk.append(token)
+        
+        
+        op_stk.reverse()
+        num_stk.reverse()
+        
+        while op_stk:
+            op = op_stk.pop()
+            left, right = num_stk.pop(), num_stk.pop()
+            num_stk.append(left + right if op == "+" else left - right)
+            
+        return num_stk[0]
+        
+    def tokenize(self, s: str) -> List[str]:
+        res = []
+        cur = -1
+        
+        for c in s:
+            if c.isspace():
+                continue
+            elif c.isdigit():
+                cur = cur * 10 + int(c) if cur != -1 else int(c)
+            else:
+                if cur != -1:
+                    res.append(str(cur))
+                    cur = -1
+                
+                res.append(c)
+                
+        return res + [str(cur)] if cur != -1 else res
+
+
+# Solution 2
 class Solution:
     def calculate(self, s: str) -> int:
         stk, cur, op = [], 0, "+"
@@ -37,6 +86,7 @@ class Solution:
                     stk.append(stk.pop() * cur)
                 elif op == "/":
                     stk.append(int(stk.pop() / cur))
+                
                 cur, op = 0, s[i]
 
         return sum(stk)
