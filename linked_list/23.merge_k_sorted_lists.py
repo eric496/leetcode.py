f4cf0201247d194841671c72262f1ea7c708e457
eric496 <eric.mlengineer@gmail.com>
2020-06-15 11:24:25 -0400

    Modify solution

--- linked_list/23.merge_k_sorted_lists.py
+++ linked_list/23.merge_k_sorted_lists.py
@@ -13,7 +13,7 @@ Output: 1->1->2->3->4->4->5->6
 
 """
 Thought process:
-
+    Divide and Conquer - same as merge sort
 """
 
 
@@ -25,38 +25,38 @@ class ListNode:
 
 
 # Solution 1: Merge sort
-class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+class Solution(object):
+    def mergeKLists(self, lists) -> ListNode:
         if not lists:
             return None
-
+        
         if len(lists) == 1:
             return lists[0]
-
+        
         mid = len(lists) // 2
         left = self.mergeKLists(lists[:mid])
         right = self.mergeKLists(lists[mid:])
-
-        return self.mergeSort(left, right)
-
-    def mergeSort(self, ls1: List[ListNode], ls2: List[ListNode]) -> ListNode:
-        if None in (ls1, ls2):
-            return ls1 or ls2
-
-        sentinel = walk = ListNode(None)
-
-        while ls1 and ls2:
-            if ls1.val < ls2.val:
-                walk.next = ls1
-                ls1 = ls1.next
+        
+        return self.merge(left, right)
+    
+    def merge(self, left: ListNode, right: ListNode) -> ListNode:
+        if not left or not right:
+            return left or right
+            
+        sentinel = walk = ListNode(-1)
+        
+        while left and right:
+            if left.val < right.val:
+                walk.next = left
+                left = left.next
             else:
-                walk.next = ls2
-                ls2 = ls2.next
-
+                walk.next = right
+                right = right.next
+            
             walk = walk.next
-
-        walk.next = ls1 or ls2
-
+            
+        walk.next = left or right
+        
         return sentinel.next
 
 
