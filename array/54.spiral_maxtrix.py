0d4637311a054584e16877fe108e1cca3bdf9478
eric496 <eric.mlengineer@gmail.com>
2020-06-20 23:01:01 -0400

    Modify solution

--- array/54.spiral_maxtrix.py
+++ array/54.spiral_maxtrix.py
@@ -23,10 +23,33 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:
+            return []
+        
         res = []
-
-        while matrix:
-            res.extend(matrix.pop(0))
-            matrix = list(zip(*matrix))[::-1]
-
+        m, n = len(matrix), len(matrix[0])
+        row_start = col_start = 0
+        row_end, col_end = m - 1, n - 1
+        d = 0
+        
+        while row_start <= row_end and col_start <= col_end:
+            if d == 0:
+                for i in range(col_start, col_end + 1):
+                    res.append(matrix[row_start][i])
+                row_start += 1
+            elif d == 1:
+                for i in range(row_start, row_end + 1):
+                    res.append(matrix[i][col_end])
+                col_end -= 1
+            elif d == 2:
+                for i in range(col_end, col_start - 1, -1):
+                    res.append(matrix[row_end][i])
+                row_end -= 1
+            elif d == 3:
+                for i in range(row_end, row_start - 1, -1):
+                    res.append(matrix[i][col_start])
+                col_start += 1
+            
+            d = (d + 1) % 4
+            
         return res
