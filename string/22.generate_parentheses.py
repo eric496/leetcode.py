b6c2cb7ea1be73665e92cf1fdc295e53ee4b8ece
eric496 <eric.mlengineer@gmail.com>
2020-06-29 12:51:59 -0400

    Modify solution

--- string/22.generate_parentheses.py
+++ string/22.generate_parentheses.py
@@ -12,9 +12,36 @@ For example, given n = 3, a solution set is:
 """
 
 
+# Solution 1
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        if not n:
+        if n <= 0:
+            return []
+        
+        res = []
+        self.dfs(0, 0, n, "", res)
+        
+        return res
+        
+    def dfs(self, left: int, right: int, n: int, cur: str, res: List[str]) -> None:
+        if right > left:
+            return 
+        
+        if left == right == n:
+            res.append(cur)
+            return
+        
+        if left < n:
+            self.dfs(left + 1, right, n, cur + "(", res)
+            
+        if right < n:
+            self.dfs(left, right + 1, n, cur + ")", res)
+
+
+# Solution 2: decrease n we can save one parameter
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n <= 0:
             return []
 
         res = []
@@ -26,7 +53,7 @@ class Solution:
         if right < left:
             return
 
-        if not left and not right:
+        if left == right == 0:
             res.append(path)
             return
 
