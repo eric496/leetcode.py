8f9343fe156740038c2573b2debd1b45be22b5aa
eric496 <eric.mlengineer@gmail.com>
2020-07-12 18:28:31 -0400

    Add 1512

---
+++ math/1512.number_of_good_pairs.py
@@ -0,0 +1,39 @@
+"""
+Given an array of integers nums.
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+Return the number of good pairs.
+
+Example 1:
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+Example 2:
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+
+Example 3:
+Input: nums = [1,2,3]
+Output: 0
+
+Constraints:
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+"""
+
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        cnt = {}
+        
+        for num in nums:
+            cnt[num] = cnt.get(num, 0) + 1
+            
+        res = 0
+        
+        for v in cnt.values():
+            res += v * (v - 1) // 2
+            
+        return res
+        
