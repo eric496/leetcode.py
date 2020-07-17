a57cbaa74a2a67276a1741099a90df9572cfd6cf
eric496 <eric.mlengineer@gmail.com>
2020-07-17 12:32:53 -0400

    Modify solution

--- dfs/695.max_area_of_island.py
+++ dfs/695.max_area_of_island.py
@@ -20,29 +20,34 @@ Note: The length of each dimension in the given grid does not exceed 50.
 """
 
 
+# Solution 1: DFS
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid:
+        if not grid or not grid[0]:
             return 0
 
-        areas = []
+        m, n = len(grid), len(grid[0])
+        res = 0
+        visited = set()
 
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col]:
-                    areas.append(self.dfs(grid, row, col))
-
-        return max(areas) if areas else 0
-
-    def dfs(self, grid: List[List[int]], row, col) -> int:
-        if 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col]:
-            grid[row][col] = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] and (i, j) not in visited:
+                    area = self.dfs(grid, i, j, visited)
+                    res = max(res, area)
+        
+        return res
+    
+    def dfs(self, grid: List[List[int]], y: int, x: int, visited: set) -> int:
+        if 0 <= y < len(grid) and 0 <= x < len(grid[0]) and grid[y][x] and (y, x) not in visited:
+            visited.add((y, x))
+            
             return (
-                self.dfs(grid, row + 1, col)
-                + self.dfs(grid, row - 1, col)
-                + self.dfs(grid, row, col + 1)
-                + self.dfs(grid, row, col - 1)
+                self.dfs(grid, y + 1, x, visited)
+                + self.dfs(grid, y - 1, x, visited)
+                + self.dfs(grid, y, x + 1, visited)
+                + self.dfs(grid, y, x - 1, visited)
                 + 1
             )
-
+        
         return 0
