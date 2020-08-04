92cc56821d484dd72cadcf5da76f6fb04c9e48b1
eric496 <eric.mlengineer@gmail.com>
2020-08-04 10:40:05 -0400

    Modify solution

--- math/342.power_of_four.py
+++ math/342.power_of_four.py
@@ -10,4 +10,4 @@ Follow up: Could you solve it without loops/recursion?
 
 class Solution:
     def isPowerOfFour(self, num: int) -> bool:
-        return num > 0 and (num & (num - 1)) == 0 and (num - 1) % 3 == 0
+        return num > 0 and num & num - 1 == 0 and (num - 1) % 3 == 0
