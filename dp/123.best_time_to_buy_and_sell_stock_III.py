9b3a0a2b9978d45bd241e42e1b4859dc4f94b8b3
eric496 <eric.mlengineer@gmail.com>
2020-08-16 20:42:06 -0400

    Add solution

--- dp/123.best_time_to_buy_and_sell_stock_III.py
+++ dp/123.best_time_to_buy_and_sell_stock_III.py
@@ -21,3 +21,21 @@ Input: [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
 """
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        
+        n = len(prices)
+        dp = [[0] * nn for _ in range(3)]
+        
+        for k in range(1, 3):
+            curmin = prices[0]
+            
+            for i in range(1, n):
+                curmin = min(curmin, prices[i] - dp[k-1][i-1])
+                dp[k][i] = max(dp[k][i-1], prices[i] - curmin)
+                
+        return dp[2][n-1]
