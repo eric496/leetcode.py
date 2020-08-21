3b7c5ee6bf645593a4835540ea24724f29bdafee
eric496 <eric.mlengineer@gmail.com>
2020-08-21 12:05:34 -0400

    Modify solution

--- two_pointers/905.sort_array_by_parity.py
+++ two_pointers/905.sort_array_by_parity.py
@@ -1,17 +1,13 @@
 """
 Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
-
 You may return any answer array that satisfies this condition.
 
 Example 1:
-
 Input: [3,1,2,4]
 Output: [2,4,3,1]
 The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
 
-
 Note:
-
 1 <= A.length <= 5000
 0 <= A[i] <= 5000
 """
@@ -19,14 +15,14 @@ Note:
 
 class Solution:
     def sortArrayByParity(self, A: List[int]) -> List[int]:
-        start, end = 0, len(A) - 1
-
-        while start < end:
-            if A[start] % 2 != 0 and A[end] % 2 == 0:
-                A[start], A[end] = A[end], A[start]
-            elif A[start] % 2 == 0:
-                start += 1
-            elif A[end] % 2 != 0:
-                end -= 1
-
+        lo, hi = 0, len(A) - 1
+        
+        while lo < hi:
+            if A[lo] & 1 and not A[hi] & 1:
+                A[lo], A[hi] = A[hi], A[lo]
+            elif not A[lo] & 1:
+                lo += 1
+            elif A[hi] & 1:
+                hi -= 1
+                
         return A
