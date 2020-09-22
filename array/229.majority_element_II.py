ec32b7e1199fd09ff32aeb7543c058c44fdc1550
eric496 <eric.mlengineer@gmail.com>
2020-09-22 10:17:27 -0400

    Add 1094

--- array/229.majority_element_II.py
+++ array/229.majority_element_II.py
@@ -1,6 +1,5 @@
 """
 Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
-
 Note: The algorithm should run in linear time and in O(1) space.
 
 Example 1:
@@ -15,31 +14,22 @@ Output: [1,2]
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        if not nums:
-            return []
-
+        me1 = me2 = None
         cnt1 = cnt2 = 0
-        can1 = can2 = None
-
-        for n in nums:
-            if n == can1:
+        
+        for num in nums:
+            if num == me1:
                 cnt1 += 1
-            elif n == can2:
+            elif num == me2:
                 cnt2 += 1
             elif cnt1 == 0:
-                cnt1, can1 = 1, n
+                me1 = num
+                cnt1 = 1
             elif cnt2 == 0:
-                cnt2, can2 = 1, n
+                me2 = num
+                cnt2 = 1
             else:
-                cnt1, cnt2 = cnt1 - 1, cnt2 - 1
-
-        freq1, freq2 = nums.count(can1), nums.count(can2)
-
-        if freq1 > len(nums) / 3 and freq2 > len(nums) / 3:
-            return [can1, can2]
-        elif freq1 > len(nums) / 3:
-            return [can1]
-        elif freq2 > len(nums) / 3:
-            return [can2]
-        else:
-            return []
+                cnt1 -= 1
+                cnt2 -= 1
+        
+        return [me for me in (me1, me2) if nums.count(me) > len(nums)//3]
