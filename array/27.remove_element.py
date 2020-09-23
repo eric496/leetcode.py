d76104bd61a214548a6d7ff665a37991af14d02f
eric496 <eric.mlengineer@gmail.com>
2020-09-22 22:54:28 -0400

    Modified solution

--- array/27.remove_element.py
+++ array/27.remove_element.py
@@ -4,27 +4,22 @@ Do not allocate extra space for another array, you must do this by modifying the
 The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
 Example 1:
-
 Given nums = [3,2,2,3], val = 3,
 Your function should return length = 2, with the first two elements of nums being 2.
 It doesn't matter what you leave beyond the returned length.
 
 Example 2:
-
 Given nums = [0,1,2,2,3,0,4,2], val = 2,
 Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
 Note that the order of those five elements can be arbitrary.
 It doesn't matter what values are set beyond the returned length.
 
 Clarification:
-
 Confused why the returned value is an integer but your answer is an array?
 Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
 Internally you can think of this:
-
 // nums is passed in by reference. (i.e., without making a copy)
 int len = removeElement(nums, val);
-
 // any modification to nums in your function would be known by the caller.
 // using the length returned by your function, it prints the first len elements.
 for (int i = 0; i < len; i++) {
@@ -39,12 +34,16 @@ Thought:
 
 
 class Solution:
-    def removeElement(self, nums: list, val: int) -> int:
-        if nums == []:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        if not nums:
             return 0
-        tail = 0
-        for n in nums:
-            if n != val:
-                nums[tail] = n
-                tail += 1
-        return tail
+        
+        n = len(nums)
+        pos = 0
+         
+        for num in nums:
+            if num != val:
+                nums[pos] = num
+                pos += 1
+        
+        return pos
