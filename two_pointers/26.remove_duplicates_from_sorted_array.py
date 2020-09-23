f1919a5aa05d0bff8fd069872e2e653a15354f50
eric496 <eric.mlengineer@gmail.com>
2020-09-22 22:46:17 -0400

    Modified solution

--- two_pointers/26.remove_duplicates_from_sorted_array.py
+++ two_pointers/26.remove_duplicates_from_sorted_array.py
@@ -3,26 +3,21 @@ Given a sorted array nums, remove the duplicates in-place such that each element
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
 Example 1:
-
 Given nums = [1,1,2],
 Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
 It doesn't matter what you leave beyond the returned length.
 
 Example 2:
-
 Given nums = [0,0,1,1,1,2,2,3,3,4],
 Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
 It doesn't matter what values are set beyond the returned length.
 
 Clarification:
-
 Confused why the returned value is an integer but your answer is an array?
 Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
 Internally you can think of this:
-
 // nums is passed in by reference. (i.e., without making a copy)
 int len = removeDuplicates(nums);
-
 // any modification to nums in your function would be known by the caller.
 // using the length returned by your function, it prints the first len elements.
 for (int i = 0; i < len; i++) {
@@ -30,24 +25,30 @@ for (int i = 0; i < len; i++) {
 }
 """
 
-"""
-Thought:
-    Use two pointers. One pointer points to the tail of the array with only unique elements. 
-    The other pointer loop through the array, replacing the current tail with new unique element but ignoring the duplicated element.
-    Increment the tail pointer if new unique element was inserted. 
-"""
-
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        n = len(nums) 
+        pos = 1
+        
+        for i in range(1, n):
+            if nums[i] != nums[i-1]:
+                nums[pos] = nums[i]
+                pos += 1
+                
+        return pos
 
-        pt = 0
 
-        for n in nums[1:]:
-            if n != nums[pt]:
-                nums[pt + 1] = n
-                pt += 1
-
-        return pt + 1
+# Solution 2: count duplicates
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        cnt = 0
+        n = len(nums)
+        
+        for i in range(1, n):
+            if nums[i] == nums[i-1]:
+                cnt += 1
+            else:
+                nums[i-cnt] = nums[i]
+                
+        return n - cnt
