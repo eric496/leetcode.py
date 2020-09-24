0cada96e2c7a3f0b8a0f82a781631f3b66d334dc
eric496 <eric.mlengineer@gmail.com>
2020-09-24 10:08:11 -0400

    Modified solution

---
+++ two_pointers/125.valid_palindrome.py
@@ -0,0 +1,36 @@
+"""
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+Input: "A man, a plan, a canal: Panama"
+Output: true
+
+Example 2:
+Input: "race a car"
+Output: false
+"""
+
+
+# Two pointers
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) < 2:
+            return True
+        
+        i, j = 0, len(s) - 1
+        
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+                
+            while i < j and not s[j].isalnum():
+                j -= 1
+                                
+            if s[i].lower() != s[j].lower():
+                return False
+            
+            i += 1
+            j -= 1
+            
+        return True
