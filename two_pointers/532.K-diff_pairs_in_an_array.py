867f97620d7b169dbacbe6de41618b7dbf4f9bb5
eric496 <eric.mlengineer@gmail.com>
2020-10-07 10:23:25 -0400

    Add 532

---
+++ two_pointers/532.K-diff_pairs_in_an_array.py
@@ -0,0 +1,64 @@
+"""
+Given an array of integers nums and an integer k, return the number of unique k-diff pairs in the array.
+A k-diff pair is an integer pair (nums[i], nums[j]), where the following are true:
+0 <= i, j < nums.length
+i != j
+|nums[i] - nums[j]| == k
+Notice that |val| denotes the absolute value of val.
+
+Example 1:
+Input: nums = [3,1,4,1,5], k = 2
+Output: 2
+Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+Although we have two 1s in the input, we should only return the number of unique pairs.
+
+Example 2:
+Input: nums = [1,2,3,4,5], k = 1
+Output: 4
+Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5).
+
+Example 3:
+Input: nums = [1,3,1,5,4], k = 0
+Output: 1
+Explanation: There is one 0-diff pair in the array, (1, 1).
+
+Example 4:
+Input: nums = [1,2,4,4,3,3,0,9,2,3], k = 3
+Output: 2
+
+Example 5:
+Input: nums = [-1,-2,-3], k = 1
+Output: 2
+ 
+Constraints:
+1 <= nums.length <= 104
+-107 <= nums[i] <= 107
+0 <= k <= 107
+"""
+
+
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        if len(nums) < 2:
+            return 0
+        
+        nums.sort()
+        i = j = 0
+        res = 0
+        n = len(nums)
+        
+        while i < n and j < n:
+            if i == j or nums[i] + k > nums[j]:
+                j += 1
+            elif nums[i] + k < nums[j]:
+                i += 1
+            else:
+                res += 1
+                
+                while j < n - 1 and nums[j] == nums[j+1]:
+                    j += 1
+                
+                i += 1
+                j += 1
+            
+        return res
