01833902d1804c0cac8f10d45fa210450c60883b
eric496 <eric.mlengineer@gmail.com>
2020-10-07 10:36:54 -0400

    Modify solution

--- sliding_window/567.permutation_in_string.py
+++ sliding_window/567.permutation_in_string.py
@@ -18,27 +18,24 @@ The length of both given strings is in range [1, 10,000].
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-
         cnt = [0] * 26
-
-        for i in range(len(s1)):
-            cnt[ord(s1[i]) - ord("a")] += 1
-            cnt[ord(s2[i]) - ord("a")] -= 1
-
-        if not any(cnt):
-            return True
-
-        end = len(s1)
-
-        while end < len(s2):
-            cnt[ord(s2[end]) - ord("a")] -= 1
-            cnt[ord(s2[end - len(s1)]) - ord("a")] += 1
-
-            if not any(cnt):
+        
+        for c in s1:
+            cnt[ord(c)-ord("a")] += 1
+            
+        left = right = 0
+        n = len(s2)
+        
+        while right < n:
+            cnt[ord(s2[right])-ord("a")] -= 1
+            
+            while left < n and -1 in cnt:
+                cnt[ord(s2[left])-ord("a")] += 1
+                left += 1
+                
+            if sum(cnt) == 0:
                 return True
-
-            end += 1
-
+            
+            right += 1
+        
         return False
