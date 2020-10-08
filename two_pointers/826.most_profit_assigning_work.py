2a20bd9291c651a426bf46cbf7c41841624d2278
eric496 <eric.mlengineer@gmail.com>
2020-10-08 10:38:57 -0400

    Modify solution

--- two_pointers/826.most_profit_assigning_work.py
+++ two_pointers/826.most_profit_assigning_work.py
@@ -68,17 +68,17 @@ class Solution:
 
 # Solution 2: two pointers
 class Solution:
-    def maxProfitAssignment(
-        self, difficulty: List[int], profit: List[int], worker: List[int]
-    ) -> int:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
         jobs = sorted(zip(difficulty, profit))
+        worker.sort()
         res = i = best = 0
-
-        for w in sorted(worker):
-            while i < len(jobs) and w >= jobs[i][0]:
+        n = len(jobs)
+        
+        for w in worker:
+            while i < n and w >= jobs[i][0]:
                 best = max(best, jobs[i][1])
                 i += 1
-
+                
             res += best
-
+        
         return res
