ba93e88b3834474f61a5dd5101c368e8627a8274
eric496 <eric.mlengineer@gmail.com>
2020-11-13 09:41:48 -0500

    Modify solution

--- tree/117.populating_next_right_pointers_in_each_node_II.py
+++ tree/117.populating_next_right_pointers_in_each_node_II.py
@@ -14,12 +14,12 @@ Input: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"r
 Output: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":null,"right":null,"val":7},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"6","left":null,"next":null,"right":{"$ref":"5"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"6"},"val":1}
 Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B.
 
-Note:
+Follow up:
 You may only use constant extra space.
 Recursive approach is fine, implicit stack space does not count as extra space for this problem.
 """
 
-"""
+
 # Definition for a Node.
 class Node:
     def __init__(self, val, left, right, next):
@@ -27,7 +27,37 @@ class Node:
         self.left = left
         self.right = right
         self.next = next
-"""
+
+
+# Level order traversal
+from collections import deque
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        
+        q = deque([root])
+        
+        while q:
+            size = len(q)
+            
+            for i in range(size):
+                node = q.popleft()
+                
+                if i == size - 1:
+                    node.next = None
+                else:
+                    node.next = q[0]
+                
+                if node.left:
+                    q.append(node.left)
+                    
+                if node.right:
+                    q.append(node.right)
+                    
+        return root
+
 
 # Recursive
 class Solution:
