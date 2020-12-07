b4387bbcca252b128cfcc03182729acea6f79bc8
eric496 <eric.mlengineer@gmail.com>
2020-12-07 10:54:25 -0500

    Modify solution

--- array/54.spiral_maxtrix.py
+++ array/54.spiral_maxtrix.py
@@ -22,34 +22,39 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix or not matrix[0]:
-            return []
-
-        res = []
-        m, n = len(matrix), len(matrix[0])
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        res = [[0] * n for _ in range(n)]
+        
+        if n == 0:
+            return res
+        
         row_start = col_start = 0
-        row_end, col_end = m - 1, n - 1
-        d = 0
-
+        row_end = col_end = n - 1
+        fill = 1
+        
         while row_start <= row_end and col_start <= col_end:
-            if d == 0:
-                for i in range(col_start, col_end + 1):
-                    res.append(matrix[row_start][i])
-                row_start += 1
-            elif d == 1:
-                for i in range(row_start, row_end + 1):
-                    res.append(matrix[i][col_end])
-                col_end -= 1
-            elif d == 2:
-                for i in range(col_end, col_start - 1, -1):
-                    res.append(matrix[row_end][i])
-                row_end -= 1
-            elif d == 3:
-                for i in range(row_end, row_start - 1, -1):
-                    res.append(matrix[i][col_start])
-                col_start += 1
-
-            d = (d + 1) % 4
-
+            for j in range(col_start, col_end+1):
+                res[row_start][j] = fill
+                fill += 1
+            
+            row_start += 1
+            
+            for i in range(row_start, row_end+1):
+                res[i][col_end] = fill
+                fill += 1
+            
+            col_end -= 1
+            
+            for j in range(col_end, col_start-1, -1):
+                res[row_end][j] = fill
+                fill += 1
+                
+            row_end -= 1
+            
+            for i in range(row_end, row_start-1, -1):
+                res[i][col_start] = fill
+                fill += 1
+                
+            col_start += 1
+            
         return res
