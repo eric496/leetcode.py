00933e6acb87f288ee378f16766c5fb8544020ba
Eric Wang <erwng@amazon.com>
2026-03-04 09:43:28 +0000

    Update solution

--- greedy/826.most_profit_assigning_work.py
+++ greedy/826.most_profit_assigning_work.py
@@ -17,53 +17,26 @@ difficulty[i], profit[i], worker[i]  are in range [1, 10^5]
 """
 
 
-# Solution 1: binary search
-from collections import defaultdict
-
-
+# Solution 1: greedy
 class Solution:
-    def maxProfitAssignment(
-        self, difficulty: List[int], profit: List[int], worker: List[int]
-    ) -> int:
-        cur_max = float("-inf")
-        aux = [(d, p) for d, p in zip(difficulty, profit)]
-        aux.sort()
-        difficulty.sort()
-        mp = defaultdict(int)
-
-        for d, p in aux:
-            cur_max = max(cur_max, p)
-            if d in mp:
-                if cur_max > mp[d]:
-                    mp[d] = cur_max
-            else:
-                mp[d] = cur_max
-
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        jobs = [(p, d) for p, d in zip(profit, difficulty)]
+        jobs.sort(reverse=True)
+        worker.sort(reverse=True)
         res = 0
+        i = 0
+        n = len(jobs)
 
         for w in worker:
-            print(self.lower_bound(difficulty, w))
-            res += mp[self.lower_bound(difficulty, w)]
-
-        return res
-
-    def lower_bound(self, difficulty: List[int], target: int) -> int:
-        if target < difficulty[0]:
-            return 0
-        elif target > difficulty[-1]:
-            return difficulty[-1]
-
-        lo, hi = 0, len(difficulty)
-
-        while lo < hi:
-            mid = lo + (hi - lo >> 1)
-
-            if difficulty[mid] >= target:
-                hi = mid
+            while i < n and w < jobs[i][1]:
+                i += 1
+            
+            if i < n:
+                res += jobs[i][0]
             else:
-                lo = mid + 1
+                break
 
-        return difficulty[lo] if difficulty[lo] <= target else difficulty[lo - 1]
+        return res 
 
 
 # Solution 2: two pointers
