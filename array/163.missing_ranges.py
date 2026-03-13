34f4347eda69a2fe6a8c5aed8a5cb74e28dce6df
Eric Wang <erwng@amazon.com>
2026-03-13 07:29:32 +0000

    Update problem description and solution

--- array/163.missing_ranges.py
+++ array/163.missing_ranges.py
@@ -1,21 +1,41 @@
 """
-Given a sorted integer array nums, where the range of elements are in the inclusive range [lower, upper], return its missing ranges.
+You are given an inclusive range [lower, upper] and a sorted unique integer array nums, where all elements are within the inclusive range.
+A number x is considered missing if x is in the range [lower, upper] and x is not in nums.
+Return the shortest sorted list of ranges that exactly covers all the missing numbers. That is, no element of nums is included in any of the ranges, and each missing number is covered by one of the ranges.
 
-Example:
-Input: nums = [0, 1, 3, 50, 75], lower = 0 and upper = 99,
-Output: ["2", "4->49", "51->74", "76->99"]
+Example 1:
+Input: nums = [0,1,3,50,75], lower = 0, upper = 99
+Output: [[2,2],[4,49],[51,74],[76,99]]
+Explanation: The ranges are:
+[2,2]
+[4,49]
+[51,74]
+[76,99]
+
+Example 2:
+Input: nums = [-1], lower = -1, upper = -1
+Output: []
+Explanation: There are no missing ranges since there are no missing numbers.
+
+Constraints:
+    -109 <= lower <= upper <= 109
+    0 <= nums.length <= 100
+    lower <= nums[i] <= upper
+    All the values of nums are unique.
 """
 
 
 class Solution:
-    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
-        nums = [lower - 1] + nums + [upper + 1]
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
         res = []
+        prev = lower - 1
 
-        for i in range(len(nums) - 1):
-            if nums[i + 1] - nums[i] == 2:
-                res.append(str(nums[i] + 1))
-            elif nums[i + 1] - nums[i] > 2:
-                res.append(str(nums[i] + 1) + "->" + str(nums[i + 1] - 1))
+        for num in nums:
+            if prev + 1 <= num - 1:
+                res.append([prev+1, num-1])
+            prev = num
 
+        if prev + 1 <= upper:
+            res.append([prev+1, upper])
+        
         return res
